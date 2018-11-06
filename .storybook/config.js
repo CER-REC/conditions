9484557680d3d18d5d5744c4b86e8274c49ce91d
Brandon Devaleriola <brandon.devaleriola@vizworx.com>
2018-11-06 14:15:52 -0700

    fixed jsx lint error and added linter config to ignore dev-dependencies for stroybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -34,7 +34,7 @@ configureViewport({
 
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../stories', true, /.stories.jsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
--- stories/index.stories.jsx
+++ stories/index.stories.jsx
@@ -3,9 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { checkA11y } from '@storybook/addon-a11y';
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
 import { withNotes } from '@storybook/addon-notes';
-import { configureViewport } from '@storybook/addon-viewport';
 import { Button, Welcome } from '@storybook/react/demo';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
