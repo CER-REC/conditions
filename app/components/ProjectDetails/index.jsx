52fe6ed37325243b9190dfc8afc19bade32c653b
Spenser Jones <hello@spenserj.com>
2018-11-15 11:12:48 -0700

    docs(Component Template): Replace placeholder components with template
    NEBV-1149

--- app/components/ProjectDetails/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './styles.scss';
-
-export default React.memo(() => null);
---
+++ app/components/_template/index.stories.jsx
@@ -0,0 +1,11 @@
+/*
+import React from 'react';
+import { storiesForComponent } from '../../../.storybook/utils';
+import MyComponentName from './';
+import ReadMe from './README.md';
+
+storiesForComponent('Components|MyComponentName', module, ReadMe)
+  .add('default', () => (
+    <MyComponentName />
+  ));
+*/
