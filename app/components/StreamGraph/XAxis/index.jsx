72d3c29134b963a9c43b40d058f83751f1c71db8
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2018-11-19 16:24:01 -0700

    NEBV-1178 tests added for x-axis

--- app/components/StreamGraph/XAxis/index.jsx
+++ app/components/StreamGraph/XAxis/index.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const XAxis = (props) => {
+  if (props.dateRange.length === 0) { return null; }
+  const dateRange = [];
   return (
-    <div className="x-axis" />
+    <div className="x-axis">
+      {dateRange}
+    </div>
   );
 };
 
 XAxis.propsTypes = {
-  dateRange: PropTypes.arrayOf(PropTypes.string),
+  dateRange: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+XAxis.defaultProps = {
+  dateRange: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
 };
 
 export default XAxis;
--- app/components/StreamGraph/XAxis/index.spec.jsx
+++ app/components/StreamGraph/XAxis/index.spec.jsx
@@ -13,7 +13,22 @@ describe('Components|X Axis', () => {
     });
   });
 
+  describe('with default props', () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<XAxis dateRange={[2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017]} />);
+    });
 
+    it('should render', () => {
+      expect(wrapper.type()).to.equal('div');
+    });
 
+    it('should have an X Axis class', () => {
+      expect(wrapper.is('div.x-axis')).to.equal(true);
+    });
 
+    // it('should render 8 dates', () => {
+    //   expect(wrapper.find('')).to.have.lengthOf(8);
+    // });
+  });
 });
