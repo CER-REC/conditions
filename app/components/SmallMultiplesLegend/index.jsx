bf9ae93bfc394bcdcca15f9637bc8ac0944c4b12
Alan Chen <alan.chen@vizworx.com>
2018-11-27 11:05:44 -0700

    feat(SmallMultiplesLegend): Added rendering of the legend items
    NEBV-1177

--- app/components/SmallMultiplesLegend/index.jsx
+++ app/components/SmallMultiplesLegend/index.jsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import List from '../List';
+import SmallMultiplesLegendItem from './SmallMultiplesLegendItem';
 
 const SmallMultiplesLegend = (props) => {
+  let legendList;
+  let legendDataItems = [];
+
+  if (props.data) {
+    legendDataItems = props.data.map(conditionsData => (
+      <SmallMultiplesLegendItem title={conditionsData.id} data={conditionsData.conditions} />
+    ));
+  }
+
+  if (legendDataItems.length) {
+    let legendItems = [];
+
+    if (legendDataItems.length > 1) {
+      const allItem = <SmallMultiplesLegendItem title={props.allLabel} />;
+
+      legendItems = [allItem];
+    }
+
+    legendItems = legendItems.concat(legendDataItems);
+    // TODO: Update List properties when the vertical feature is implemented
+    legendList = <List items={legendItems} selected={0} />;
+  }
+
   return (
     <div>
       <span>{props.title}</span>
-      <span>[TODO: Change to vertical list]</span>
+      {legendList}
     </div>
   );
 };
 
 SmallMultiplesLegend.propTypes = {
   title: PropTypes.string.isRequired,
+  allLabel: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    conditions: PropTypes.arrayOf(PropTypes.shape({
+      date: PropTypes.instanceOf(Date).isRequired,
+      number: PropTypes.number.isRequired,
+    })).isRequired,
+  })),
 };
 
-SmallMultiplesLegend.defaultProps = {};
+SmallMultiplesLegend.defaultProps = {
+  allLabel: 'All',
+  data: null,
+};
 
 export default SmallMultiplesLegend;
