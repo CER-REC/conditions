aadc26f4fe4801ae0649885bd5c96dc1dd47ef6e
Alan Chen <alan.chen@vizworx.com>
2018-12-05 09:57:36 -0700

    test(SmallMultiplesLegend): Added tests for LegendItem unhighlight
    NEBV-1177

--- app/components/SmallMultiplesLegend/LegendItem/index.spec.jsx
+++ app/components/SmallMultiplesLegend/LegendItem/index.spec.jsx
@@ -13,6 +13,32 @@ describe('Components|SmallMultiplesLegend/LegendItem', () => {
     });
   };
 
+  const itShouldNotRenderGraph = () => {
+    it('should not render the graph');
+  };
+
+  const itShouldRenderGraph = () => {
+    it('should render the graph');
+  };
+
+  const itShouldRenderWithoutUnhighlight = () => {
+    it('should render without the unhighlight class', () => {
+      // eslint-disable-next-line no-unused-expressions
+      expect(wrapper.hasClass('LegendItem')).to.be.true;
+      // eslint-disable-next-line no-unused-expressions
+      expect(wrapper.hasClass('unhighlight')).to.be.false;
+    });
+  };
+
+  const itShouldRenderWithUnhighlight = () => {
+    it('should render with the unhighlight class', () => {
+      // eslint-disable-next-line no-unused-expressions
+      expect(wrapper.hasClass('LegendItem')).to.be.true;
+      // eslint-disable-next-line no-unused-expressions
+      expect(wrapper.hasClass('unhighlight')).to.be.true;
+    });
+  };
+
   describe('when only the title property is provided', () => {
     const title = 'Test Title';
 
@@ -21,11 +47,23 @@ describe('Components|SmallMultiplesLegend/LegendItem', () => {
     });
 
     itShouldRenderTitle(title);
-
-    it('should not render the graph');
+    itShouldNotRenderGraph();
+    itShouldRenderWithoutUnhighlight();
   });
 
-  describe('when only the data property is provided', () => {
+  describe('when the title and unhighlight property is provided', () => {
+    const title = '123 ABC test_title';
+
+    beforeEach(() => {
+      wrapper = shallow(<LegendItem title={title} unhighlight />);
+    });
+
+    itShouldRenderTitle(title);
+    itShouldNotRenderGraph();
+    itShouldRenderWithUnhighlight();
+  });
+
+  describe('when the title and data property is provided', () => {
     const title = '(<{}>)other_test-title.!?';
     const data = [{
       date: new Date('2018-01-01T00:00:00.000Z'),
@@ -43,7 +81,29 @@ describe('Components|SmallMultiplesLegend/LegendItem', () => {
     });
 
     itShouldRenderTitle(title);
+    itShouldRenderGraph();
+    itShouldRenderWithoutUnhighlight();
+  });
 
-    it('should render the graph');
+  describe('when the title, data, and unhighlight property is provided', () => {
+    const title = '';
+    const data = [{
+      date: new Date('2018-01-04T00:00:00.000Z'),
+      number: 11,
+    }, {
+      date: new Date('2018-04-08T00:00:00.000Z'),
+      number: 22,
+    }, {
+      date: new Date('2018-08-16T00:00:00.000Z'),
+      number: 33,
+    }];
+
+    beforeEach(() => {
+      wrapper = shallow(<LegendItem title={title} data={data} unhighlight />);
+    });
+
+    itShouldRenderTitle(title);
+    itShouldRenderGraph();
+    itShouldRenderWithUnhighlight();
   });
 });
