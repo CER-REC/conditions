a5f7ee06f4530a22898d0ad5db68f6fa8b2516c7
axzs <Johnsgaard.J@gmail.com>
2018-12-10 13:04:58 -0700

    refactor(Project Menu): Removes utility for sorting
    NEBV-1176

--- app/components/ProjectMenu/ProjectChart/index.jsx
+++ app/components/ProjectMenu/ProjectChart/index.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import shortChartData from '../utitlities';
 import FeatureFlag from './FeatureFlag';
 
-const ProjectChart = (props) => {
-  const sortedData = shortChartData(props.chartType, props.graphData);
-  return (
-    <div className="ProjectChart">
-      { sortedData.map(condition => (
-        <FeatureFlag
-          key={condition.name}
-          count={condition.count}
-          color={condition.color}
-        />
-      ))}
-    </div>
-  );
-};
+const ProjectChart = props => (
+  <div className="ProjectChart">
+    { props.graphData.map(condition => (
+      <FeatureFlag
+        key={condition.name}
+        count={condition.count}
+        color={condition.color}
+      />
+    ))}
+  </div>
+);
 
 ProjectChart.propTypes = {
-  /** The selected feature */
-  chartType: PropTypes.string.isRequired,
   /** All of the projects condition data */
   graphData: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
