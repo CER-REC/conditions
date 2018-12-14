eeaa474a48d6ecc209550826343eb0676d5aaaef
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2018-12-14 13:26:03 -0700

    feat(feature description): added styles
    NEBV-1190

--- app/components/FeatureDescription/index.jsx
+++ app/components/FeatureDescription/index.jsx
@@ -4,18 +4,27 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const FeatureDescription = (props) => {
-  if (props.description === '') { return null; }
+  if (props.description === '' || props.feature === '') { return null; }
   return (
-    <div className="feature-description" />
+    <div
+      className="feature-description"
+      role="document"
+      aria-label={`${props.feature} description`}
+    >
+      <h1>{props.feature}</h1>
+      {props.description}
+    </div>
   );
 };
 
 FeatureDescription.propTypes = {
   description: PropTypes.string,
+  feature: PropTypes.string,
 };
 
 FeatureDescription.defaultProps = {
   description: '',
+  feature: '',
 };
 
 export default FeatureDescription;
--- app/components/FeatureDescription/index.spec.jsx
+++ app/components/FeatureDescription/index.spec.jsx
@@ -5,18 +5,20 @@ import { expect } from 'chai';
 import FeatureDescription from './';
 
 const description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ullamcorper elit. Nulla vitae molestie mauris. Nulla placerat ullamcorper quam a ornare. Ut tempor orci sed arcu faucibus, eu mollis turpis lobortis. Vivamus fermentum neque id tincidunt sagittis. Morbi blandit orci eu augue semper pellentesque. Aenean eleifend quis quam id rhoncus. Etiam tristique hendrerit elit, sit amet tempor lacus gravida vitae. Proin viverra erat sed hendrerit convallis.';
+const feature = 'Feature title';
 
 describe('Components|FeatureDescription', () => {
   describe('with no props', () => {
     it('should not render', () => {
-      expect(<FeatureDescription description="" />);
+      const wrapper = shallow(<FeatureDescription description="" feature="" />);
+      expect(wrapper.type()).to.equal(null);
     });
   });
 
   describe('with props', () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = shallow(<FeatureDescription description={description} />);
+      wrapper = shallow(<FeatureDescription description={description} feature={feature} />);
     });
 
     it('should render', () => {
@@ -25,6 +27,10 @@ describe('Components|FeatureDescription', () => {
 
     it('should have a feature description class', () => {
       expect(wrapper.is('.feature-description')).to.equal(true);
+    });
+
+    it('should have a heading', () => {
+      expect(wrapper.find('h1')).to.have.lengthOf(1);
     });
   });
 });
