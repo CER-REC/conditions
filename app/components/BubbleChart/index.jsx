b03934ae52165bd59d446f97bfd4773d9e1de4e3
Manali Bhattacharyya <manali@Manalis-MacBook-Air.local>
2018-12-16 21:58:04 -0700

    feat(BubbleChart): tests, readme and setup
    Initial tests, 2svg rendering with calculations, and storybook setup
    
    Closes NEBV-1232

--- app/components/BubbleChart/index.jsx
+++ app/components/BubbleChart/index.jsx
@@ -2,13 +2,87 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import handleInteraction from '../../utilities/handleInteraction';
 
-const BubbleChart = (props) => {
-};
+class BubbleChart extends React.PureComponent {
+  static propTypes = {
+    selectedCategory: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+  }
 
-BubbleChart.propTypes = {
-};
+  setCircles = (data) => {
+    const placedCircles = [];
+    let theta = 45;
+    // eslint-disable-next-line no-restricted-syntax
+    data.forEach((circle, index) => {
+      if (index === 0) {
+        placedCircles.push({
+          name: circle.name,
+          radius: circle.radius,
+          x: circle.radius,
+          y: circle.radius,
+        });
+      }
+      if (index === 1) {
+        let placed = false;
+        const tempIndex = 0;
+        while (placed !== true) {
+          const circ1 = placedCircles[tempIndex];
+          const circ2 = circle;
+          const circ2x = circ1.x + ((circ2.radius + circ1.radius) * Math.cos(theta)); // need to add circ1.x
+          const circ2y = circ1.y + ((circ2.radius + circ1.radius) * Math.sin(theta)); // need to add circ1.y
+          // check if it doesn't exceed the svg below
+          const bottomCirclePoint = ((circ2.radius) * Math.sin(270)) + circ2y;
+          if (this.props.height > bottomCirclePoint) {
+            placed = true;
+            placedCircles.push({
+              name: circle.name,
+              radius: circle.radius,
+              x: circ2x,
+              y: circ2y,
+            });
+          } else {
+            theta -= 5;
+          }
+        }
+      }
+      console.log(placedCircles);
+    });
+    return placedCircles.map(i => <circle r={i.radius} cx={i.x} cy={i.y} />);
+  }
 
-BubbleChart.defaultProps = {
-};
+
+  determineArea = () => {
+    const data = this.props.energyBubbleData;
+    const maxEnergyData = data.reduce((a, b) => ({ energyNum: a.energyNum + b.energyNum }));
+    const maxEnergyNumber = maxEnergyData.energyNum;
+    const minimumSpace =
+    (this.props.height > this.props.width) ? this.props.width : this.props.height;
+    const areaData = data.map(obj =>
+      ({ name: obj.name, area: ((obj.energyNum / maxEnergyNumber) * minimumSpace * 0.9) }));
+    return this.determineRadius(areaData);
+  }
+  determineRadius = (areaData) => {
+    const radiusData = areaData.map(obj =>
+      ({ name: obj.name, radius: Math.sqrt(obj.area / Math.PI) }));
+    return this.setCircles(radiusData);
+  }
+
+  renderSvg = () => {
+    // eslint-disable-next-line no-unused-expressions
+    <svg width={this.props.width} height={this.props.height}>
+      {(this.determineArea())}
+    </svg >;
+  }
+
+  render() {
+    if (this.props.selectedCategory !== 'instrument') {
+      return null;
+    }
+    return (
+      <div className="bubbleChart">
+        { this.renderSvg()}
+      </div>);
+  }
+}
 
 export default BubbleChart;
--- app/components/BubbleChart/index.stories.jsx
+++ app/components/BubbleChart/index.stories.jsx
@@ -1,5 +1,33 @@
 
 import React from 'react';
 import { storiesForComponent } from '../../../.storybook/utils';
-import List from './';
+import BubbleChart from './';
 import ReadMe from './README.md';
+
+const energyBubbleData = [
+  {
+    name: 'gas',
+    energyNum: 100,
+  },
+  {
+    name: 'power',
+    energyNum: 30,
+  },
+  {
+    name: 'oil',
+    energyNum: 100,
+  },
+  {
+    name: 'not specified',
+    energyNum: 50,
+  },
+  {
+    name: 'any commodity types',
+    energyNum: 150,
+  },
+];
+
+storiesForComponent('Components|BubbleChart', module, ReadMe)
+  .add('default', () => (
+    <BubbleChart selectedCategory="instrument" width={1000} height={1500} energyBubbleData={energyBubbleData} />
+  ));
