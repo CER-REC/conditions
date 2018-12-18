42e715d709ac539f8e39acfcd6dc3e4166e14977
Alan Chen <alan.chen@vizworx.com>
2018-12-18 11:53:00 -0700

    docs(FeaturesMenu): Added examples to storybook
    NEBV-1236

--- app/components/FeaturesMenu/index.stories.jsx
+++ app/components/FeaturesMenu/index.stories.jsx
@@ -5,5 +5,17 @@ import ReadMe from './README.md';
 
 storiesForComponent('Components|FeaturesMenu', module, ReadMe)
   .add('basic usage', () => (
-    <FeaturesMenu />
+    <FeaturesMenu
+      title="Title"
+      features={['Feature 1', 'Feature 2', 'Feature 3', 'Feature 4', 'Feature 5']}
+      onChange={feature => alert(feature)}
+    />
+  ))
+  .add('drop down', () => (
+    <FeaturesMenu
+      title="Title"
+      features={['TEST ABC', 'TEST 123', 'test']}
+      onChange={feature => alert(feature)}
+      dropDown
+    />
   ));
