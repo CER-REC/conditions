9dccb6ec3a81b8c5c0318b506ac9b5133da95d31
Alan Chen <alan.chen@vizworx.com>
2018-12-18 11:49:37 -0700

    feat(FeaturesMenu): Added rendering of the component for drop down and no drop down modes
    NEBV-1236

--- app/components/FeaturesMenu/index.jsx
+++ app/components/FeaturesMenu/index.jsx
@@ -1,14 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import List from '../List';
 
-const FeaturesMenu = (props) => {
+// TODO: This is a mock, replace with the translation function
+const t = searchList => searchList[2];
+
+const getListMenu = (features, onChange) => (
+  // TODO: Update List properties when the vertical feature is implemented
+  <List
+    items={features}
+    selected={0}
+    onChange={index => onChange(features[index])}
+  />
+);
+
+const getDropDownMenu = (features, onChange) => {
+  const options = features.map(feature => (
+    <option key={feature} value={feature}>{feature}</option>
+  ));
+
   return (
-    <div></div>
+    <select onChange={event => onChange(event.target.value)}>
+      {options}
+    </select>
   );
 };
 
-FeaturesMenu.propTypes = {};
+const FeaturesMenu = (props) => {
+  const features = props.features.map(feature => t(['featuresMenu', 'title', feature]));
+  const { onChange, dropDown } = props;
+  const menu = dropDown ? getDropDownMenu(features, onChange) : getListMenu(features, onChange);
 
-FeaturesMenu.defaultProps = {};
+  return (
+    <div className={`FeaturesMenu ${dropDown ? 'dropDown' : ''} ${props.className}`}>
+      <span className="title">{`${t(['featuresMenu', 'title', props.title])}`}</span>
+      {menu}
+    </div>
+  );
+};
+
+FeaturesMenu.propTypes = {
+  /** The title to be displayed */
+  title: PropTypes.string.isRequired,
+  /** The list of features to display in the menu */
+  features: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  /** The flag to determine if the component renders in drop down mode */
+  dropDown: PropTypes.bool,
+  /** A function that will receive an feature when a new feature is selected */
+  onChange: PropTypes.func.isRequired,
+  /** Additional className to add to the component */
+  className: PropTypes.string,
+};
+
+FeaturesMenu.defaultProps = {
+  dropDown: false,
+  className: '',
+};
 
 export default FeaturesMenu;
