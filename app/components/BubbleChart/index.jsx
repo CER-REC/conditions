bbfde1e6fd2f0a903c8750e5fb095209025a7e22
Manali Bhattacharyya <manali.bhattacharyya@vizworx.com>
2018-12-19 14:48:43 -0700

    refactor(InstrumentBubble): Refactored Code to use data as props
    Closes NEBV-1234

--- app/components/BubbleChart/index.jsx
+++ app/components/BubbleChart/index.jsx
@@ -1,86 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import InstrumentBubble from '../BubbleChart/InstrumentBubble/index'
-
-// Placeholder (will be moved to props completely afterwards)
-const bubbleChartData = {
-  name: 'data',
-  children: [{
-    parentName: 'GAS',
-    children: [
-      {
-        name: 'XG',
-        children: [],
-      }, {
-        name: 'GC',
-        children: [],
-        value: 50,
-      },
-      {
-        name: 'GPSO',
-        children: [],
-        value: 25,
-      },
-      {
-        name: 'SG',
-        children: [],
-        value: 40,
-      },
-      {
-        name: 'GPLO',
-        children: [],
-        value: 50,
-      }],
-  },
-  {
-    parentName: 'POWER',
-    children: [
-      {
-        name: 'EC',
-        children: [],
-        value: 50,
-      },
-      {
-        name: 'EPE',
-        children: [],
-        value: 25,
-      },
-    ]
-},
-  {
-    parentName: 'OIL',
-    children: [{
-      name: 'XO',
-      children: [],
-      value: 25,
-    },
-    {
-      name: 'SO',
-      children: [],
-      value: 50,
-    }, {
-      name: 'OC',
-      children: [],
-      value: 75,
-    }, {
-      name: 'OPL',
-      children: [],
-      value: 25,
-    }, {
-      name: 'OPLO',
-      children: [],
-      value: 25,
-    }, {
-      name: 'OPSO',
-      children: [],
-      value: 25,
-    }],
-  }],
-};
+import InstrumentBubble from '../BubbleChart/InstrumentBubble/index';
 
 class BubbleChart extends React.PureComponent {
   static propTypes = {
     selectedCategory: PropTypes.string.isRequired,
+    bubbleChartData: PropTypes.instanceOf(Object).isRequired,
   }
 
   render() {
@@ -89,7 +14,7 @@ class BubbleChart extends React.PureComponent {
     }
     return (
       <div className="bubbleChart">
-        <InstrumentBubble bubbleChartData={bubbleChartData} />
+        <InstrumentBubble instrumentChartData={this.props.bubbleChartData} />
       </div>);
   }
 }
