c55d44c08f5e984ac245db5c02a3bb1189570ffe
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-01-02 17:38:48 -0700

    feat(InstrumentBubble): Changed Component Structure
    Altered from using d3 for everything to using only d3 hierarchy and svg
    for rendering
    
    Closes NEBV-1234

--- app/components/BubbleChart/InstrumentBubble/index.jsx
+++ app/components/BubbleChart/InstrumentBubble/index.jsx
@@ -1,100 +1,124 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as d3 from 'd3';
+import handleInteraction from '../../../utilities/handleInteraction';
+
+/* To do:
+1) Check if the text is overlapping another and change arc accordingly
+2) Check if the inside text is smaller than circle radius, and shift the text
+3) Add to onClick function to mimic design document functionality
+*/
+
+const pickColor = (category) => {
+  let colorSelected;
+  switch (category) {
+    case 'routing':
+      colorSelected = '#0E2B8C';
+      break;
+    case 'construction':
+      colorSelected = '#27A5F9';
+      break;
+    case 'abandonment':
+      colorSelected = '#164EF8';
+      break;
+    case 'misc':
+      colorSelected = '#D4A92A';
+      break;
+    case 'safety':
+      colorSelected = '#C904C2';
+      break;
+    case 'tariffs':
+      colorSelected = '#C3E6B3';
+      break;
+    case 'opening':
+      colorSelected = '#6AE6B2';
+      break;
+    default:
+      colorSelected = 'white';
+  }
+  return colorSelected;
+};
 
 class InstrumentBubble extends React.PureComponent {
   static propTypes = {
     instrumentChartData: PropTypes.instanceOf(Object).isRequired, // Placeholder
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
   }
 
-  componentDidMount() {
-    this.fauxDOMRender(null, this.props.instrumentChartData);
-  }
-
-  setRef(componentNode) {
-    this.rootNode = componentNode;
-  }
-
-  fauxDOMRender(err, data) {
-    const node = this.rootNode;
-    const d = 650;
-    const svg = d3.select(node).append('svg').attr('width', 650).attr('height', 650);
-
-    const d3onClickFunction = (clickData, i, clickNodes) => {
-      d3.selectAll(clickNodes).style('stroke', 'transparent');
-      d3.select(clickNodes[i]).style('stroke', 'magenta').style('stroke-width', '2');
-      svg.selectAll('path').remove();
-      svg.append('g').append('path').attr('d', 'M 5 5 L 15 5 L 10 15 z').attr('transform', `translate(${clickData.x})`);
-      // To do: Add dashed line from the arrow to the circle
-    };
-
-    // D3 Pack Layout used for circle packing
-    const pack = d3.pack().size([d, d]).padding(10).radius(r => r.value);
-
-    // Root Node created through sum of total data
-    const root = d3.hierarchy(data)
-      .sum(totalData => totalData.value).sort((a, b) => (b.value - a.value));
-
-    // Children nodes (energy bubbls + individual instrument bubbles)
-    const nodes = svg.datum(data)
-      .selectAll('g')
-      .data(pack(root).descendants())
-      .enter()
-      .append('g')
-      .attr('transform', transformD => `translate(${transformD.x}, ${transformD.y})`);
-
-    // Circle attached to each individual children node
-    nodes.append('circle')
-      .each((circleData, i, circleNodes) => d3.select(circleNodes[i])
-        .attr('r', (circleData.r))
-        .style('fill', 'orange') // To do: Alter the color to change with category
-        .style('stroke', 'transparent')
-        .attr('tabindex', '0')
-        .attr('fill-opacity', circleData.children ? 0.25 : 1.0) // To do: Remove property when colors are changed
-        .attr('stroke', circleData.children ? 'rgb(31, 119, 180)' : 'none'))
-
-      // onClick function to remove previous styles and apply for the selected element
-      .on('click', (clickData, i, clickNodes) => {
-        d3onClickFunction(clickNodes, i, clickData);
+  circleRender = (nodes, onClick) => (
+    nodes.filter(v => (v.depth !== 0))
+      .map((node) => {
+        // Renders parent circles (ie Energy bubbles)
+        if (node.depth === 1) {
+          // Create curved path for parentNode text
+          const textCurvedPath = `M ${(node.x - node.r)},${(node.y - 1)} A ${node.r},${node.r} 0 0,1 ${(node.x + node.r)},${(node.y - 1)}`;
+          return (
+            <g key={node.data.parentName}>
+              <path id={`${node.data.parentName}path`} d={textCurvedPath} style={{ fill: 'none', stroke: 'transparent' }} />
+              <circle
+                {...handleInteraction(onClick)}
+                transform={`translate(${node.x} ${node.y})`}
+                r={node.r}
+                tabIndex="0"
+                style={{
+                  fill: 'transparent',
+                  stroke: '#EDEDED',
+                  strokeWidth: 2,
+                }}
+              />
+              <text>
+                <textPath href={`#${node.data.parentName}path`} textAnchor="middle" startOffset="50%">
+                  {node.data.parentName}
+                </textPath>
+              </text>
+            </g>
+          );
+        }
+        // Nested Children circles (ie Instruments)
+        return (
+          <g
+            key={node.data.name}
+          >
+            <circle
+              {...handleInteraction(onClick)}
+              r={node.r}
+              stroke="none"
+              transform={`translate(${node.x} ${node.y})`}
+              tabIndex="0"
+              style={{ fill: pickColor(node.data.category), stroke: 'transparent' }}
+            />
+            <text x={node.x} y={node.y} textAnchor="middle">{node.data.name}</text>
+          </g>
+        );
       })
-      // keyPress function
-      .on('keypress', (keypressData, i, keypressNodes) => {
-        if (d3.event.keyCode === 13) {
-          d3onClickFunction(keypressData, i, keypressNodes);
-        }
-      });
+  )
 
-    // Display Names for parent (energy bubbles)
-    nodes.filter(parentData => parentData.children)
-      .append('text')
-      .attr('dy', filteredData => filteredData.r)
-      .text((parentData) => {
-        if (parentData.data.parentName !== undefined) {
-          return parentData.data.parentName.substring(0, parentData.r);
-        }
-        return null;
-      });
-
-    // Display names for children (instrument bubbles)
-    nodes.filter(childrenData => !childrenData.children)
-      .append('text')
-      .attr('ref', 'check')
-      .attr('dy', '0.5em')
-      .style('text-anchor', 'middle')
-      .style('font', '8px sans-serif')
-      .text(childrenData => childrenData.data.name.substring(0, d.r))
-      .attr('class', 'textRender')
-      .on('click', (clickData, i, clickNodes) => {
-        // To do: Render once to get computedTextLength > and reRender with appropriate circle sizes
-        console.log(clickNodes[i].getComputedTextLength());
-        // Will be used to grab the text length (during first render) and create
-        // a second circle with the text rendering
-      });
+  d3HierarchyCalculation() {
+    const { width, height, instrumentChartData } = this.props;
+    //  d3 pack generates a function to fit data into tightly packed circles
+    const pack = d3.pack()
+      .size([width, height])
+      .padding(node => (node.depth === 0 ? 0 : 10))
+      .radius(r => r.value);
+    // creates the root node using d3 hierarchy similar to a tree layout
+    const root = d3.hierarchy(instrumentChartData)
+      .sum(totalData => totalData.value)
+      .sort((a, b) => (b.value - a.value));
+    return pack(root).descendants();
   }
 
   render() {
+    const { width, height, onClick } = this.props;
+    const d3Calculation = this.d3HierarchyCalculation();
+
     return (
-      <div className="InstrumentBubble" ref={this.setRef.bind(this)} />);
+      <div className="InstrumentBubble">
+        <svg width={width} height={height}>
+          {this.circleRender(d3Calculation, onClick)}
+        </svg>
+      </div>
+    );
   }
 }
 
