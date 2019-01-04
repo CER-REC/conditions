8c101d4d30b19b0fe86e4b26dca18602938b2399
Spenser Jones <hello@spenserj.com>
2019-01-04 14:00:08 -0700

    chore(Component Structure): removes \`index.\` from spec and stories filenames
    Now that the standard for components is to nest private components in
    their own folder, it doesn't make sense to use the `index.` prefix for
    spec and stories.

--- .storybook/config.jsx
+++ .storybook/config.jsx
@@ -30,9 +30,9 @@ addDecorator(withOptions({
 }));
 addDecorator(storyFn => <div className="visualization">{storyFn()}</div>);
 
-// automatically import all files ending in *.stories.js
-const documentationStories = require.context('../documentation/', true, /.stories.jsx$/);
-const componentStories = require.context('../app/', true, /.stories.jsx$/);
+// automatically import all files named stories.jsx
+const documentationStories = require.context('../documentation/', true, /stories.jsx$/);
+const componentStories = require.context('../app/', true, /stories.jsx$/);
 function loadStories() {
   documentationStories.keys()
     // Sorting Documentation|Introduction to the top
