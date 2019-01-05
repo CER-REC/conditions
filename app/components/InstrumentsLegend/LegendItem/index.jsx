8dda5e3e92092650708efd7b3cd082f8318d9342
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2019-01-04 17:27:04 -0700

    feat(InstrumentsLegend): Added rendering of the LegendItem private component
    NEBV-1189

--- app/components/InstrumentsLegend/LegendItem/index.jsx
+++ app/components/InstrumentsLegend/LegendItem/index.jsx
@@ -1,15 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.scss';
+
+// TODO: This is a mock, replace with the translation function
+const t = (searchList) => {
+  if (searchList[1] === 'all') {
+    return `All ${searchList[2]}s`;
+  }
+
+  return searchList[2];
+};
 
 const LegendItem = (props) => {
+  let indicators;
+  const type = props.all ? 'all' : 'title';
+
+  if (!props.all) {
+    indicators = props.indicators.map((indicator) => {
+      const style = indicator ? { backgroundColor: props.color } : {};
+
+      return <div className="indicator" style={style} />;
+    });
+  }
+
   return (
-    <div></div>
+    <div className={`LegendItem ${props.className}`}>
+      <span className="indicators">
+        {indicators}
+      </span>
+      <span>{t(['instrumentsLegend', type, props.title])}</span>
+    </div>
   );
 };
 
-LegendItem.propTypes = {};
+LegendItem.propTypes = {
+  /** The text beside the indicators */
+  title: PropTypes.string.isRequired,
+  /** The list of flags to toggle the display of the indicators */
+  indicators: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  /** The color of the indicators */
+  color: PropTypes.string.isRequired,
+  /** The flag to determine if the component renders as a all filter item */
+  all: PropTypes.bool,
+  /** Additional className to add to the component */
+  className: PropTypes.string,
+};
 
-LegendItem.defaultProps = {};
+LegendItem.defaultProps = {
+  all: false,
+  className: '',
+};
 
 // TODO: Wrap in React.memo when testing issue fixed
 export default LegendItem;
