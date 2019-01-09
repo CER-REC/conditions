4a6620e4e37da3ce4163e01751c750488a256aaf
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-01-09 10:33:23 -0700

    refactor(Controller): refactored tests and code
    Changed it to seperate svgs into inline svgs and added knobs in tests
    
    Issues Affected: NEBV-1260

--- app/components/Controller/stories.jsx
+++ app/components/Controller/stories.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { withKnobs, number } from '@storybook/addon-knobs';
 import { storiesForComponent } from '../../../.storybook/utils';
-import withStatus from '../../../.storybook/addon-status';
 import Controller from '.';
 import ReadMe from './README.md';
 
+const options = {
+  range: true,
+  min: -300,
+  max: 300,
+  step: 100,
+};
+
+const heightOptions = {
+  range: true,
+  min: 0,
+  max: 200,
+  step: 20,
+};
 storiesForComponent('Components|Controller', module, ReadMe)
+  .addDecorator(withKnobs)
   .add('default', () => (
-    <Controller x={80} ystart={50} yend={150} />
+    <Controller x={number('x position(px)', 50, options)} ystart={number('y start position(px)', 20, heightOptions)} yend={number('y end position(px)', 40, heightOptions)} />
   ));
