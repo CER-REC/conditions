c82912593f023c9e5821f73f3d5f246b4508febd
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-01-10 10:39:03 -0700

    test(Controller): added texts to improve coverage
    Issues Affected: NEBV-1260

--- app/components/Controller/spec.jsx
+++ app/components/Controller/spec.jsx
@@ -7,11 +7,10 @@ describe('Components|Controller', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Controller x={50} ystart={50} yend={150} radius={20} text="123" />,
+      <Controller x={50} ystart={50} yend={150} />,
     );
   });
-
-  describe('with props', () => {
+  describe('with default props', () => {
     it('should render a div', () => {
       expect(wrapper.type()).to.equal('div');
     });
@@ -19,13 +18,56 @@ describe('Components|Controller', () => {
     it('should have a classname of Controller', () => {
       expect(wrapper.find('.Controller')).to.have.lengthOf(1);
     });
-
-    it('should render svg with proper calculated height', () => {
-      expect(wrapper.find('svg').prop('height')).to.equal(240);
+    it('should render an svg ', () => {
+      expect(wrapper.find('svg')).to.have.lengthOf(1);
     });
-
-    it('should translate the component with passed in props', () => {
-      expect(wrapper.find('svg').prop('transform')).to.equal('translate(50, 50)');
+    it('should render a path', () => {
+      expect(wrapper.find('path')).to.have.lengthOf(1);
     });
+    it('should render a line', () => {
+      expect(wrapper.find('line')).to.have.lengthOf(1);
+    });
+  });
+});
+describe('without radius', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Controller x={50} ystart={50} yend={150} />,
+    );
+  });
+  it('should not render a circle', () => {
+    expect(wrapper.exists('circle')).to.equal(false);
+  });
+
+  it('should have svg height excluding diameter', () => {
+    expect(wrapper.find('svg').prop('height')).to.equal(200);
+  });
+
+  it('should have svg width excluding diameter', () => {
+    expect(wrapper.find('svg').prop('width')).to.equal(20);
+  });
+});
+describe('with radius', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Controller x={50} ystart={40} yend={100} radius={40} />,
+    );
+  });
+  it('should render a circle', () => {
+    expect(wrapper.find('circle')).to.have.lengthOf(1);
+  });
+
+  it('should render a circle with proper radius', () => {
+    expect(wrapper.find('circle').prop('r')).to.equal(40);
+  });
+
+  it('should have svg height including diameter', () => {
+    expect(wrapper.find('svg').prop('height')).to.equal(220);
+  });
+
+  it('should have svg witdth including diameter', () => {
+    expect(wrapper.find('svg').prop('width')).to.equal(80);
   });
 });
--- app/components/Controller/stories.jsx
+++ app/components/Controller/stories.jsx
@@ -20,8 +20,8 @@ const heightOptions = {
 storiesForComponent('Components|Controller', module, ReadMe)
   .addDecorator(withKnobs)
   .add('withCircle', () => (
-    <Controller text="123" x={number('x position(px)', 50, options)} ystart={number('y start position(px)', 20, heightOptions)} yend={number('y end position(px)', 40, heightOptions)} radius={number('radius(px)', 40, heightOptions)} />
+    <Controller x={number('x position(px)', 50, options)} ystart={number('y start position(px)', 20, heightOptions)} yend={number('y end position(px)', 40, heightOptions)} radius={number('radius(px)', 40, heightOptions)} />
   ))
   .add('withoutCircle', () => (
-    <Controller text="123" x={number('x position(px)', 50, options)} ystart={number('y start position(px)', 20, heightOptions)} yend={number('y end position(px)', 40, heightOptions)} />
+    <Controller x={number('x position(px)', 50, options)} ystart={number('y start position(px)', 20, heightOptions)} yend={number('y end position(px)', 40, heightOptions)} />
   ));
