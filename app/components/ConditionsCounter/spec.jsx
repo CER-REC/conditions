867c98fa400f2120ffb6b39e75d157305e4965f6
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-01-10 15:35:38 -0700

    refactor(Controller): changed component name, style changes
    Issues Affected: NEBV-1260

--- app/components/ConditionsCounter/spec.jsx
+++ app/components/ConditionsCounter/spec.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import Controller from '.';
+import ConditionsCounter from '.';
 
-describe('Components|Controller', () => {
+describe('Components|ConditionsCounter', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Controller x={50} ystart={50} yend={150} />,
+      <ConditionsCounter x={50} ystart={50} yend={150} />,
     );
   });
   describe('with default props', () => {
-    it('should render a div', () => {
-      expect(wrapper.type()).to.equal('div');
-    });
-
     it('should have a classname of Controller', () => {
       expect(wrapper.find('.Controller')).to.have.lengthOf(1);
-    });
-    it('should render an svg ', () => {
-      expect(wrapper.find('svg')).to.have.lengthOf(1);
     });
     it('should render a path', () => {
       expect(wrapper.find('path')).to.have.lengthOf(1);
@@ -33,26 +26,18 @@ describe('without radius', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Controller x={50} ystart={50} yend={150} />,
+      <ConditionsCounter x={50} ystart={50} yend={150} />,
     );
   });
   it('should not render a circle', () => {
     expect(wrapper.exists('circle')).to.equal(false);
-  });
-
-  it('should have svg height excluding diameter', () => {
-    expect(wrapper.find('svg').prop('height')).to.equal(200);
-  });
-
-  it('should have svg width excluding diameter', () => {
-    expect(wrapper.find('svg').prop('width')).to.equal(20);
   });
 });
 describe('with radius', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <Controller x={50} ystart={40} yend={100} radius={40} />,
+      <ConditionsCounter x={50} yTop={40} yBottom={100} radius={40} />,
     );
   });
   it('should render a circle', () => {
@@ -61,13 +46,5 @@ describe('with radius', () => {
 
   it('should render a circle with proper radius', () => {
     expect(wrapper.find('circle').prop('r')).to.equal(40);
-  });
-
-  it('should have svg height including diameter', () => {
-    expect(wrapper.find('svg').prop('height')).to.equal(220);
-  });
-
-  it('should have svg witdth including diameter', () => {
-    expect(wrapper.find('svg').prop('width')).to.equal(80);
   });
 });
