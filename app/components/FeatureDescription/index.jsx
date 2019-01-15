7aba890c767be9be513673ba60f5b1c5895a27d3
Johnsgaards <hello@gaard.ca>
2019-01-15 13:31:10 -0700

    feat(Language Support): adds multi language support to the Feature Description component
    Issues Affected: NEBV-1262

--- app/components/FeatureDescription/index.jsx
+++ app/components/FeatureDescription/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 
 import './styles.scss';
 
@@ -9,13 +10,25 @@ const FeatureDescription = props => (
     role="document"
     aria-label={`${props.feature} description`}
   >
-    <h1>{props.feature}</h1>
-    {props.description}
+    <h1>
+      <FormattedMessage id={`common.features.${props.feature}`}>
+        {text => text}
+      </FormattedMessage>
+    </h1>
+    <FormattedMessage id={props.description}>
+      { (text) => {
+        const stringArr = text.split('\n');
+        // eslint-disable-next-line react/no-array-index-key
+        return stringArr.map((string, index) => <p key={index}>{string}</p>);
+      }}
+    </FormattedMessage>
   </div>
 );
 
 FeatureDescription.propTypes = {
+  /** Path to description in translation file ex.(components.projectsOverview.description) */
   description: PropTypes.string.isRequired,
+  /** The selected feature in the feature menu */
   feature: PropTypes.string.isRequired,
 };
 
