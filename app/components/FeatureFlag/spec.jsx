28d5c789057e1fa44315096835a43c1ba897fce7
Johnsgaards <hello@gaard.ca>
2019-01-16 13:36:04 -0700

    feat(Language Support): adds language support to the Feature flag title attribute
    Issues Affected: NEBV-1262

--- app/components/FeatureFlag/spec.jsx
+++ app/components/FeatureFlag/spec.jsx
@@ -9,10 +9,10 @@ describe('Components|ProjectMenu/ProjectChart/FeatureFlag', () => {
     let wrapper;
     beforeEach(() => {
       wrapper = shallow(<FeatureFlag
-        chartType="Theme"
+        chartType="theme"
         color="purple"
         count={5}
-        name="Damage Prevention"
+        name="damagePrevention"
       />);
     });
 
@@ -27,30 +27,30 @@ describe('Components|ProjectMenu/ProjectChart/FeatureFlag', () => {
 
   it('should have a FlagTip with more than 11 conditions', () => {
     const wrapper = shallow(<FeatureFlag
-      chartType="Theme"
+      chartType="theme"
       color="green"
       count={15}
-      name="Environmental Protection"
+      name="environmentalProtection"
     />);
     expect(wrapper.find('.FlagTip').type()).to.equal('div');
   });
 
   it('should NOT have a FlagTip with 10 or less conditions', () => {
     const wrapper = shallow(<FeatureFlag
-      chartType="Theme"
+      chartType="theme"
       color="green"
       count={4}
-      name="Environmental Protection"
+      name="environmentalProtection"
     />);
     expect(wrapper.contains('.FlagTip')).to.equal(false);
   });
 
   it('should NOT have the flag tip if the condition count is 0', () => {
     const wrapper = shallow(<FeatureFlag
-      chartType="Theme"
+      chartType="theme"
       color="green"
       count={0}
-      name="Environmental Protection"
+      name="environmentalProtection"
     />);
     expect(wrapper.contains('.FlagTip')).to.equal(false);
   });
