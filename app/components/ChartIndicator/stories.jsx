2f48bd8037696233863e3c2b7f043d73a61b47da
Spenser Jones <hello@spenserj.com>
2019-01-22 12:28:32 -0700

    docs(ChartIndicator): tweaks story knobs to provide additional context

--- app/components/ChartIndicator/stories.jsx
+++ app/components/ChartIndicator/stories.jsx
@@ -6,54 +6,52 @@ import ReadMe from './README.md';
 
 const options = {
   range: true,
-  min: 125,
-  max: 250,
+  min: 0,
+  max: 300,
   step: 25,
 };
 
 const topOptions = {
   range: true,
-  min: 40,
+  min: 0,
   max: 100,
-  step: 20,
-};
-
-const heightOptions = {
-  range: true,
-  min: 40,
-  max: 100,
-  step: 20,
+  step: 10,
 };
 
 const bottomOptions = {
   range: true,
-  min: 40,
-  max: 100,
-  step: 20,
+  min: 100,
+  max: 300,
+  step: 10,
 };
 
-const visible = true;
+const radiusOptions = {
+  range: true,
+  min: 20,
+  max: 100,
+  step: 10,
+};
+
 storiesForComponent('Components|ChartIndicator', module, ReadMe)
   .addDecorator(withKnobs)
   .add('withCircle', () => (
-    <svg width={500} height={500}>
+    <svg width={300} height={300} style={{ border: '1px solid #000' }}>
       <ChartIndicator
         label="With Circle"
-        display={visible}
         x={number('x position(px)', 50, options)}
-        yTop={number('y start position(px)', 20, topOptions)}
-        yBottom={number('y end position(px)', 40, bottomOptions)}
-        radius={number('radius(px)', 40, heightOptions)}
+        yTop={number('y start position(px)', 30, topOptions)}
+        yBottom={number('y end position(px)', 120, bottomOptions)}
+        radius={number('radius(px)', 40, radiusOptions)}
       />
     </svg>
   ))
   .add('withoutCircle', () => (
-    <svg width={500} height={500}>
+    <svg width={300} height={300} style={{ border: '1px solid #000' }}>
       <ChartIndicator
         label="Without circle"
         x={number('x position(px)', 50, options)}
-        yTop={number('y start position(px)', 20, topOptions)}
-        yBottom={number('y end position(px)', 40, bottomOptions)}
+        yTop={number('y start position(px)', 30, topOptions)}
+        yBottom={number('y end position(px)', 120, bottomOptions)}
       />
     </svg>
   ));
