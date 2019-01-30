43523a4b14fec15cb5eca2290de9c738a2769692
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2019-01-30 16:01:01 -0700

    refactor(region condition summary): combined region condition chart and region condition summary int
    Issues Affected: NEBV-1268

--- app/components/RegionConditionSummary/README.md
+++ app/components/RegionConditionSummary/README.md
@@ -1,12 +1,15 @@
 # Region Condition Summary
 
 The Region Condition Summary is a public component used in the Location Wheel. It replaces the
-Project Menu in View 2 when Location is selected. 
+Project Menu in View 2 when Location is selected. The bars are scaled 
+at 200% horizontal and 300% vertical of the original.
 
 ## Requirements
 
 * [X] Bars should be ordered the same as the Legend
 * [X] Scale should appear on the left of the Summary box border
+* [X] Bar width should be scaled depending on number of bars shown
+* [X] No bars should be visible if no data
 
 ## Interaction Requirements
 
--- app/components/RegionConditionSummary/index.jsx
+++ app/components/RegionConditionSummary/index.jsx
@@ -2,32 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
-import RegionConditionChart from './RegionConditionChart';
+import BarContainer from '../BarContainer';
 import './styles.scss';
 
-const RegionConditionSummary = props => (
-  <div className={classNames(
-    'RegionConditionSummary',
-    props.className,
-  )}
-  >
-    <div className="ChartContainer">
-      <div className="BorderBottom" />
-      <div className="RegionConditionSummaryTitle">
-        <FormattedMessage id="components.regionConditionSummary.title" />
+const RegionConditionSummary = (props) => {
+  if (props.featureData.length === 0) { return null; }
+  const width = 350;
+  const barWidth = width / props.featureData.length;
+  const items = props.featureData.map(k => ({ value: k.count, fill: k.color }));
+  const chart = (
+    <div className={classNames(
+      'RegionConditionChart',
+      props.className,
+    )}
+    >
+      <BarContainer
+        size={barWidth}
+        items={items}
+        vert
+        scale={1}
+      />
+    </div>
+  );
+  return (
+    <div className={classNames(
+      'RegionConditionSummary',
+      props.className,
+    )}
+    >
+      <div className="ChartContainer">
+        <div className="BorderBottom" />
+        <div className="RegionConditionSummaryTitle">
+          <FormattedMessage id="components.regionConditionSummary.title" />
+        </div>
+      </div>
+      <svg className="YAxis">
+        <text x="45" y="11" fill="rgb(161, 168, 167)">100</text>
+        <rect x="85" y="0" width="10" height="3" fill="#e4e4e4" />
+        <text x="58" y="105" fill="rgb(161, 168, 167)">0</text>
+        <rect x="85" y="50" width="10" height="3" fill="#e4e4e4" />
+      </svg>
+      <div className="Chart">
+        {chart}
       </div>
     </div>
-    <svg className="YAxis">
-      <text x="45" y="11" fill="rgb(161, 168, 167)">100</text>
-      <rect x="85" y="0" width="10" height="3" fill="#e4e4e4" />
-      <text x="58" y="105" fill="rgb(161, 168, 167)">0</text>
-      <rect x="85" y="50" width="10" height="3" fill="#e4e4e4" />
-    </svg>
-    <div className="Chart">
-      <RegionConditionChart featureData={props.featureData} />
-    </div>
-  </div>
-);
+  );
+};
 
 RegionConditionSummary.propTypes = {
   className: PropTypes.string,
