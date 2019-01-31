0e1c7435d5fee4dfd7c5c5038995aac02150a46d
Adam Lovatt <adam.lovatt@vizworx.com>
2019-01-30 17:21:42 -0700

    feat(FeatureTypeDescription): show feature-specific content

--- app/components/FeatureTypesDescription/index.jsx
+++ app/components/FeatureTypesDescription/index.jsx
@@ -4,15 +4,56 @@ import { FormattedMessage } from 'react-intl';
 
 import './styles.scss';
 
-const FeatureTypeDescription = props => (
-  <div className="feature-description">
+const instrumentGroupColors = {
+  routing: '#0E2B8C',
+  construction: '#27A5F9',
+  abandonment: '#164EF8',
+  misc: '#D4A92A',
+  safety: '#C904C2',
+  tariffs: '#C3E6B3',
+  opening: '#6AE6B2',
+};
 
+const instrumentCodeGroups = {
+  opl: 'routing',
+  gpl: 'routing',
+  gc: 'construction',
+  oc: 'construction',
+};
 
-  </div>
-);
+const featureTypes = {
+  theme: [
+    'security',
+    'managementSystem',
+    'financial',
+  ],
+  instrument: [
+    'routing',
+    'construction',
+  ],
+};
+
+const FeatureTypeDescription = (props) => {
+  return (
+    <div className="feature-type-description">
+      {featureTypes[props.feature].map((type) => {
+        return (
+          <React.Fragment>
+            <FormattedMessage id={`common.${props.feature}.${type}`} tagName="h2" />
+            <FormattedMessage id={`components.featureTypeDescription.${props.feature}.${type}`} tagName="p" />
+          </React.Fragment>
+        )
+      })}
+    </div>
+  );
+};
 
 FeatureTypeDescription.propTypes = {
+  feature: PropTypes.string,
+};
 
+FeatureTypeDescription.defaultProps = {
+  feature: 'theme',
 };
 
 export default FeatureTypeDescription;
