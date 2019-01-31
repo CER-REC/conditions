b7c847150fdd5dc0ee991a7aae7ff3b60e12ab5c
Adam Lovatt <adam.lovatt@vizworx.com>
2019-01-31 10:56:45 -0700

    feat(FeatureTypeDescription): adds color coding to the Instrument codes

--- app/components/FeatureTypesDescription/stories.jsx
+++ app/components/FeatureTypesDescription/stories.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { storiesForComponent } from '../../../.storybook/utils';
+import { storiesForComponent, fixInfo } from '../../../.storybook/utils';
 // import withInteraction, { getInteractionProps } from 'storybook-addon-interaction';
 import withStatus from '../../../.storybook/addon-status';
 import FeatureTypeDescription from '.';
 import ReadMe from './README.md';
 
+fixInfo(FeatureTypeDescription);
+
 storiesForComponent('Components|FeatureTypeDescription', module, ReadMe)
   .addDecorator(withStatus('functionalityUnderDevelopment'))
   .add('default', () => (
     <FeatureTypeDescription />
+  ))
+  .add('instrument types', () => (
+    <FeatureTypeDescription feature="instrument" />
   ));
