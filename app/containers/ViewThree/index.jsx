a695c86e445eb6cf1d916abc5f3807e7853657ba
Brandon Devaleriola <brandon.devaleriola@vizworx.com>
2019-02-05 13:59:43 -0700

    docs(Architect containers):

--- app/containers/ViewThree/index.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import './styles.scss';
-
-// const ViewThree = () => <div />;
-
-// export default ViewThree;
--- app/containers/ViewTwo/index.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import './styles.scss';
-
-// const ViewTwo = () => <div />;
-
-// export default ViewTwo;
