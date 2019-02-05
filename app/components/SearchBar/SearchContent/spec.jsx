ef58d942e2b4b779aa24ce8e6be481d952260a21
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-02-05 14:55:49 -0700

    fix(SearchBar): fixed formatted message component and some tests
    Issues Affected: NEBV-1282

--- app/components/SearchBar/SearchContent/spec.jsx
+++ app/components/SearchBar/SearchContent/spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import SearchContent from '.';
 import { shallowWithIntl, shouldBehaveLikeAComponent } from '../../../tests/utilities';
 
@@ -8,29 +9,36 @@ const keywords = ['access management',
   'Assiniboine River',
   'department of', 'fracture tough'];
 
-// const exceptKeyWords = ['test1', 'test2', 'test3'];
+const noop = () => {};
 
 describe('Components|SearchBar/SearchContent', () => {
   describe('', () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = shallowWithIntl(
-        <SearchContent keywords={keywords} />,
+      wrapper = shallow(
+        <SearchContent
+          keywords={keywords}
+          exceptKeywords={keywords}
+          display
+          deleteWord={noop}
+          addWord={noop}
+          closeTab={noop}
+        />,
       );
     });
     // test('it should behave like a component', () => {
     //   shouldBehaveLikeAComponent(SearchContent, () => wrapper);
     // });
-    // test('a className of SearchContent', () => {
-    //   expect(wrapper.find('.SearchContent')).toHaveLength(1);
-    // });
+    test('a className of SearchContent', () => {
+      expect(wrapper.find('.SearchContent')).toHaveLength(1);
+    });
 
-    // test('render input', () => {
-    //   expect(wrapper.find('input')).toHaveLength(1);
-    // });
+    test('render input', () => {
+      expect(wrapper.find('input').first().hasClass('searchBar')).toBe(true);
+    });
 
-    // test('render ul list with searchWords className', () => {
-    //   expect(wrapper.find('ul.searchWords')).toHaveLength(1);
-    // });
+    test('render ul list with searchWords className', () => {
+      expect(wrapper.find('ul.searchWords').first()).toHaveLength(1);
+    });
   });
 });
