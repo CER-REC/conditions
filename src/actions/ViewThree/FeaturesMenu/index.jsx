252757dc39ebe392047c1ae27fba0b8363278b09
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2019-02-11 16:13:29 -0700

    feat(redux): adds for view three
    Issues Affected: NEBV-1285

---
+++ src/actions/ViewThree/FeaturesMenu/index.jsx
@@ -0,0 +1,8 @@
+import { Types as viewThree } from '../index';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case viewThree.SELECTED_FEATURE:
+    default: return state;
+  }
+};
