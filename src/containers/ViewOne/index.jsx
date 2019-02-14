f91e580155b87db82e529478b94d432f61bd0835
Spenser Jones <hello@spenserj.com>
2019-02-14 12:51:03 -0700

    feat(ViewOne): adds missing translations
    Issues Affected: NEBV-1263

--- src/containers/ViewOne/index.jsx
+++ src/containers/ViewOne/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { FormattedMessage } from 'react-intl';
 import ConditionExplorer from '../../components/ConditionExplorer';
 import keywords from '../../components/ConditionExplorer/mockKeywords';
 import BrowseByBtn from '../../components/BrowseByBtn';
@@ -13,9 +14,8 @@ const uniqueKeywords = keywords.filter((v, i) => keywords.indexOf(v) === i);
 const ViewOne = props => (
   <section className={classNames('ViewOne', { layoutOnly: props.layoutOnly })}>
     <section className="introduction">
-      <span style={{ fontSize: '50px', fontWeight: '700' }}>
-        CONDITIONS ON NEB-REGULATED ENERGY PROJECTS
-      </span>
+      <FormattedMessage id="views.view1.header.title" tagName="h1" />
+      <FormattedMessage id="views.view1.header.subtitle" />
     </section>
     <section className="explorer">
       <ConditionExplorer keywords={uniqueKeywords} />
