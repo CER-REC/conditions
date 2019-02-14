4f92612601c5e181e88fdb0ca99463f9898692a4
Spenser Jones <hello@spenserj.com>
2019-02-14 07:17:48 -0700

    feat(View Containers): refactors ViewOne to control components
    Issues Affected: NEBV-1263

--- src/containers/ViewOne/index.jsx
+++ src/containers/ViewOne/index.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import ConditionExplorer from '../../components/ConditionExplorer';
+import keywords from '../../components/ConditionExplorer/mockKeywords';
+import BrowseByBtn from '../../components/BrowseByBtn';
+import ShortcutInfoBar from '../../components/ShortcutInfoBar';
 import './styles.scss';
 
-const ViewOne = (props) => {
-  if (props.components.length === 0) return null;
-  const { components } = props;
-  return (
-    <section className="ViewOne">
-      <section className="a" style={{ background: '#F1F3F4' }}>
-        { components[0] }
-      </section>
-      <section className="b" style={{ background: '#F1F3F4' }}>
-        { components[1] }
-      </section>
-      <section className="c" style={{ background: '#F1F3F4' }}>
-        { components[2] }
-      </section>
-      <section className="d" style={{ background: '#F1F3F4' }}>
-        { components[3] }
-      </section>
+const noop = () => {};
+const uniqueKeywords = keywords.filter((v, i) => keywords.indexOf(v) === i);
+
+const ViewOne = props => (
+  <section className={classNames('ViewOne', { layoutOnly: props.layoutOnly })}>
+    <section className="introduction">
+      <span style={{ fontSize: '50px', fontWeight: '700' }}>
+        CONDITIONS ON NEB-REGULATED ENERGY PROJECTS
+      </span>
     </section>
-  );
-};
+    <section className="explorer">
+      <ConditionExplorer keywords={uniqueKeywords} />
+    </section>
+    <section className="browseBy">
+      <BrowseByBtn mode="company" onClick={noop} />
+      <BrowseByBtn mode="location" onClick={noop} />
+    </section>
+    <section className="infoBar">
+      <ShortcutInfoBar
+        handleInfoBar={false}
+        jumpToAbout={noop}
+        openDataModal={noop}
+        openScreenshotModal={noop}
+      />
+    </section>
+  </section>
+);
 
 ViewOne.propTypes = {
-  components: PropTypes.arrayOf(PropTypes.node).isRequired,
+  layoutOnly: PropTypes.bool,
 };
+
+ViewOne.defaultProps = {
+  layoutOnly: PropTypes.false,
+};
+
 export default ViewOne;
--- src/containers/ViewOne/stories.jsx
+++ src/containers/ViewOne/stories.jsx
@@ -1,48 +1,8 @@
 import React from 'react';
 import { storiesForView } from '../../../.storybook/utils';
 import ReadMe from './README.md';
-import BrowseByBtn from '../../components/BrowseByBtn';
-import ShortcutInfoBar from '../../components/ShortcutInfoBar';
 import ViewOne from '.';
 
-const handleInfoBar = false;
-const jumpToAbout = () => {};
-const openDataModal = () => {};
-const openScreenshotModal = () => {};
-
-const boxSizing = [
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>1</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>2&3</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>4</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>5</span></div>,
-];
-
-const components = [
-  <div><span style={{ fontSize: '50px', fontWeight: '700' }}>CONDITIONS ON NEB-REGULATED ENERGY PROJECTS</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>1</span></div>,
-  <div>
-    <BrowseByBtn
-      mode="company"
-      {...{}}
-    />
-    <BrowseByBtn
-      mode="location"
-      {...{}}
-    />
-  </div>,
-  <ShortcutInfoBar
-    onChange={() => {}}
-    handleInfoBar={handleInfoBar}
-    jumpToAbout={jumpToAbout}
-    openDataModal={openDataModal}
-    openScreenshotModal={openScreenshotModal}
-  />,
-];
-
 storiesForView('Containers|ViewOne', module, ReadMe)
-  .add('default', () => (
-    <ViewOne components={boxSizing} />
-  ))
-  .add('with components', () => (
-    <ViewOne components={components} />
-  ));
+  .add('default', () => <ViewOne />)
+  .add('layout only', () => <ViewOne layoutOnly />);
