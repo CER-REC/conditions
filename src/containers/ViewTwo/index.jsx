243f795e59970356955aeebaf707fc877bfc1cda
Spenser Jones <hello@spenserj.com>
2019-02-14 07:28:39 -0700

    feat(View Containers): refactors ViewTwo to control components
    Issues Affected: NEBV-1263

--- src/containers/ViewTwo/index.jsx
+++ src/containers/ViewTwo/index.jsx
@@ -1,32 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import ProjectMenu from '../../components/ProjectMenu';
+import FeaturesLegend from '../../components/FeaturesLegend';
+import Wheel from '../../components/Wheel';
+import { locationWheelData } from '../../components/Wheel/randomDataSample';
 import './styles.scss';
 
-const ViewTwo = (props) => {
-  if (props.components.length === 0) return null;
-  const { components } = props;
-  return (
-    <section className="ViewTwo">
-      <section className="searchHeader" style={{ background: '#F1F3F4' }}>
-        { components[0] }
-      </section>
-      <section className="wheel" style={{ background: '#F1F3F4' }}>
-        { components[1] }
-      </section>
-      <section className="companyBreakdown" style={{ background: '#F1F3F4' }}>
-        { components[2] }
-      </section>
-      <section className="menus" style={{ background: '#F1F3F4' }}>
-        { components[3] }
-      </section>
-      <section className="conditions" style={{ background: '#F1F3F4' }}>
-        { components[4] }
-      </section>
+const legendItems = [
+  { color: 'pink', description: 'security', disabled: false },
+  { color: 'green', description: 'financial', disabled: false },
+  { color: 'blue', description: 'damagePrevention', disabled: false },
+  { color: 'purple', description: 'socioEconomic', disabled: false },
+];
+
+const projectData = [
+  {
+    id: 1223,
+    name: '1. Section 21.(1) application',
+    graphData: [{ name: 'security', count: 5, color: 'pink' }, { name: 'managementSystem', count: 0, color: 'green' }],
+  },
+  {
+    id: 1224,
+    name: '2. Section 21.(1) application',
+    graphData: [{ name: 'security', count: 10, color: 'pink' }, { name: 'managementSystem', count: 19, color: 'green' }],
+  },
+  {
+    id: 1225,
+    name: '3. Section 21.(1) application',
+    graphData: [{ name: 'security', count: 4, color: 'pink' }, { name: 'managementSystem', count: 29, color: 'green' }],
+  },
+  {
+    id: 1226,
+    name: '4. Section 21.(1) application',
+    graphData: [{ name: 'security', count: 6, color: 'pink' }, { name: 'managementSystem', count: 22, color: 'green' }],
+  },
+  {
+    id: 1227,
+    name: '5. Section 21.(1) application',
+    graphData: [{ name: 'security', count: 5, color: 'pink' }, { name: 'managementSystem', count: 0, color: 'green' }],
+  },
+];
+
+const ViewTwo = props => (
+  <section className={classNames('ViewTwo', { layoutOnly: props.layoutOnly })}>
+    <section className="searchHeader">
+      <div style={{ height: '10%' }} />,
     </section>
-  );
-};
+    <section className="wheel">
+      <Wheel
+        ringType="Location"
+        itemsData={locationWheelData}
+      />
+    </section>
+    <section className="companyBreakdown">
+      <ProjectMenu
+        projectData={projectData}
+        selectedProjectID={1225}
+        onChange={() => {}}
+        selectedFeature="theme"
+      />
+    </section>
+    <section className="menus">
+      <FeaturesLegend
+        legendItems={legendItems}
+        selectedFeature="theme"
+        isProjectLegend
+      />
+    </section>
+    <section className="conditions">
+      <div style={{ height: '10%' }} />
+    </section>
+  </section>
+);
 
 ViewTwo.propTypes = {
-  components: PropTypes.arrayOf(PropTypes.node).isRequired,
+  layoutOnly: PropTypes.bool,
 };
+
+ViewTwo.defaultProps = {
+  layoutOnly: false,
+};
+
 export default ViewTwo;
--- src/containers/ViewTwo/stories.jsx
+++ src/containers/ViewTwo/stories.jsx
@@ -1,79 +1,8 @@
 import React from 'react';
 import { storiesForView } from '../../../.storybook/utils';
-import ProjectMenu from '../../components/ProjectMenu';
-import FeaturesLegend from '../../components/FeaturesLegend';
-import Wheel from '../../components/Wheel';
 import ReadMe from './README.md';
 import ViewTwo from '.';
-import { locationWheelData } from '../../components/Wheel/randomDataSample';
-
-const legendItems = [
-  { color: 'pink', description: 'security', disabled: false },
-  { color: 'green', description: 'financial', disabled: false },
-  { color: 'blue', description: 'damagePrevention', disabled: false },
-  { color: 'purple', description: 'socioEconomic', disabled: false },
-];
-
-const projectData = [
-  {
-    id: 1223,
-    name: '1. Section 21.(1) application',
-    graphData: [{ name: 'security', count: 5, color: 'pink' }, { name: 'managementSystem', count: 0, color: 'green' }],
-  },
-  {
-    id: 1224,
-    name: '2. Section 21.(1) application',
-    graphData: [{ name: 'security', count: 10, color: 'pink' }, { name: 'managementSystem', count: 19, color: 'green' }],
-  },
-  {
-    id: 1225,
-    name: '3. Section 21.(1) application',
-    graphData: [{ name: 'security', count: 4, color: 'pink' }, { name: 'managementSystem', count: 29, color: 'green' }],
-  },
-  {
-    id: 1226,
-    name: '4. Section 21.(1) application',
-    graphData: [{ name: 'security', count: 6, color: 'pink' }, { name: 'managementSystem', count: 22, color: 'green' }],
-  },
-  {
-    id: 1227,
-    name: '5. Section 21.(1) application',
-    graphData: [{ name: 'security', count: 5, color: 'pink' }, { name: 'managementSystem', count: 0, color: 'green' }],
-  },
-];
-
-const boxSizing = [
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>1</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>2</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>3</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>4</span></div>,
-  <div><span style={{ fontSize: '50px', marginLeft: '45%' }}>5</span></div>
-];
-
-const components = [
-  <div style={{ height: '10%' }} />,
-  <Wheel
-    ringType="Location"
-    itemsData={locationWheelData}
-  />,
-  <ProjectMenu
-    projectData={projectData}
-    selectedProjectID={1225}
-    onChange={() => {}}
-    selectedFeature="theme"
-  />,
-  <FeaturesLegend
-    legendItems={legendItems}
-    selectedFeature="theme"
-    isProjectLegend
-  />,
-  <div style={{ height: '10%' }} />,
-];
 
 storiesForView('Containers|ViewTwo', module, ReadMe)
-  .add('default', () => (
-    <ViewTwo components={boxSizing} />
-  ))
-  .add('with components', () => (
-    <ViewTwo components={components} />
-  ));
+  .add('default', () => <ViewTwo />)
+  .add('layout only', () => <ViewTwo layoutOnly />);
