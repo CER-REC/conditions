940f97bb3a98b31fd89e75429de5c89f60434bf0
Adam Lovatt <adam.lovatt@vizworx.com>
2019-02-15 13:09:09 -0700

    feat(ConditionDetails): updates styling, documentation, and dummy content

--- src/components/ConditionDetails/README.md
+++ src/components/ConditionDetails/README.md
@@ -1,41 +1,54 @@
-# Component ConditionDetails 
+# ConditionDetails
 
-ConditionDetails is a view of for the selected projects contains a chronological 
-list of all conditions for that project. Each bar in the list is a condition, 
-whose length corresponds to the length of the condition text. Each bar is binned 
-into short, medium, and long. Conditions are also coloured by the currently selected 
-feature. Conditions are grouped by Instrument and sorted within their instrument 
-in their original order. Instruments are sorted by their effective date from 
-earliest to latest. Each new instrument is denoted in the list by its effective 
-date; this date can be clicked to get detailed information about the instrument.
+ConditionDetails is an overview of the selected project. It contains:
+
+- A header displaying the name of the project
+- A chronological list of all conditions for that project
+- All of the content for the condition/instrument selected in the list
+- In View 3, a collapsable panel with feature details for the selected item, such as the relevant theme or instrument type
 
 ## Requirements
 
-* [ ] The component should contain a list of conditions for the project
-* [ ] The component should have a value/values for the associated bar for each condition
-* [ ] The component should a colour/colours for each condition
-* [ ] The component should change the color of the bars based on the FeaturesMenu Component
-* [ ] The component should have the condition bars grouped by instrument numbers and ordered by the effective date
-* [ ] The component should contain a list of conditions for the project
-* [ ] The component should have a value/values for the associated bar for each condition
-* [ ] The component should a colour/colours for each condition
-* [ ] The component should change the color of the bars based on the FeaturesMenu Component
-* [ ] The component should have the a textual parent denoting each instrument number which cannot be selected
-* [ ] The component should let each condition be clicked 
-* [ ] The component should let each condition highlight that it has been searched
-* [ ] The component should contain a details section
-* [ ] The component should have a details section with a list of textual key and value pairs
-* [ ] The component should finally have a 'text' key and value pair which has all of the content of the condition
-* [ ] The component should let the 'text' key have searchable text
-* [ ] The component should have a section the title of the company and a return button if in View 3
-* [ ] The component should have a more/less button if it is in the View 3
-* [ ] The component should have section for conditions features if it is in View
+### Header
+
+- Should display the name of the project
+- The name should be clickable (denoted by a pink star) to bring up a window of further project information
+- In View 3, there should be a button to toggle the Details panel open and closed
+
+### List
+
+- Uses the public List component
+- Each condition is represented by a BarContainer component, whose length corresponds to the length of the condition text. (Binned into short, medium, and long)
+- Conditions are coloured according to the currently selected feature, and can have more than one color - in this case, the bar should be split evenly among them
+- Conditions matching the current search keywords should be given a small red "tab" at one end
+- Conditions are grouped by Instrument and sorted within their instrument in their original order
+- Instruments are sorted by their effective date from earliest to latest
+- Each new instrument is denoted in the list by its effective date; this date can be clicked to get detailed information about the instrument
+
+### Content
+
+- When a condition is selected, should display the Effective Date, Instrument Number, Keywords, and condition Text
+- When an instrument is selected, should display the Issuance, Effective, and Sunset Dates, the Instrument Number, Status, and Type, the Location, and the instrument's text (Activity)
+- The Instrument Number should be highlighted and clickable, opening the Intermediate Popup component
+- Condition text (and Instrument activity?) should be split into paragraphs, with search keywords highlighted
+
+### Details
+
+(Only available in View 3)
+
+- Should list the Theme, Instrument, Phase, Type, Status, and Filing types of the current condition
+- Should be collapsable by clicking on a toggle button in the Header
+- Expanding/collapsing should be animated, with the entire ConditionDetails component sliding left to make room for the Details panel
 
 ## Interaction Requirements
 
-* Click functionality for: return button(View 3), conditions list, more/less button( View 3)
-* Mouse Scrolling for: conditions list, text details section
-* Knobs: The component should have knobs to display moving in the list and changing properties
+- Click functionality:
+  - Project name: Should opening the project details window
+  - List: Should allow different items to be selected
+  - Instrument number: Should open the Intermediate Popup window
+  - Details toggle: Should expand/collapse the details panel
+- Scrolling:
+  - The list, content, and details containers
 
 ## Accessibility Requirements
 
@@ -44,5 +57,3 @@ Tab interaction, Description and Title elements
 ## Unit Testing
 
 [ ] Implemented test suite from requirements
-
-## Examples
--- src/components/ConditionDetails/stories.jsx
+++ src/components/ConditionDetails/stories.jsx
@@ -4,6 +4,18 @@ import { storiesForComponent } from '../../../.storybook/utils';
 import ConditionDetails from '.';
 import ReadMe from './README.md';
 
+const lorem = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris facilisis est non mi euismod, eu bibendum purus varius. ',
+  'Donec mattis porta sem sed malesuada. Nulla luctus elit at suscipit tempor. Aenean maximus leo eu massa eleifend gravida. Integer nulla tortor, pulvinar quis arcu non, gravida finibus nulla. ',
+  'Quisque in velit sed odio tempor viverra quis id neque. Suspendisse id arcu sed elit feugiat bibendum eu a sem. ',
+  'Etiam tincidunt massa ut nisi dictum, vitae ultrices mi sagittis. Nulla facilisi. In convallis massa ac orci dictum semper. ',
+  'Maecenas tincidunt sem nec turpis fringilla, et gravida dui congue.',
+  'Suspendisse sed ultrices orci. Donec elementum sem aliquet, malesuada tortor at, finibus elit.',
+  'Nulla posuere aliquet nibh sit amet porta. Sed tristique arcu non consectetur euismod. Aenean maximus arcu non urna volutpat viverra.',
+  'Maecenas sit amet pretium leo, ut vestibulum arcu. Etiam in est ultricies, finibus mi non, aliquam elit. Morbi nec cursus turpis, at dictum massa.',
+  'Quisque eu lobortis velit, quis viverra elit.',
+].join('\n');
+
 const data = [
   {
     instrumentNumber: 'XO-001-2018',
@@ -13,13 +25,13 @@ const data = [
     status: 'status.inProgress',
     location: 'Albuquerque, NM',
     type: 'Construction of Oil Pipeline',
-    activity: 'IT IS ORDERED that, pursuant to section 58 of the act, the applied-for Project as specified in good lord this is putting me to sleep just typing it out',
+    activity: lorem,
     conditions: [
       {
         length: 3,
         fill: 'pink',
         keywords: ['program'],
-        text: 'blah blah blah\nhello world\nlorem ipsum dolor',
+        text: lorem,
         details: {
           theme: 'theme.environmentalProtection',
           instrument: 'instrument.category.construction',
@@ -33,7 +45,7 @@ const data = [
         length: 2,
         fill: 'blue',
         keywords: ['program'],
-        text: 'blah blah blah\nhola el mundo\nlorem ipsum dolor',
+        text: lorem,
         details: {
           theme: 'theme.environmentalProtection',
           instrument: 'instrument.category.construction',
@@ -53,13 +65,13 @@ const data = [
     status: 'status.inProgress',
     location: 'Albuquerque, NM',
     type: 'Construction of Oil Pipeline',
-    activity: 'IT IS ORDERED that, pursuant to section 58 of the act, the applied-for Project as specified in good lord this is putting me to sleep just typing it out',
+    activity: lorem,
     conditions: [
       {
         length: 1,
         fill: 'red',
         keywords: ['program'],
-        text: 'blah blah blah\nlorem ipsum dolor',
+        text: lorem,
         details: {
           theme: 'theme.environmentalProtection',
           instrument: 'instrument.category.construction',
@@ -73,7 +85,7 @@ const data = [
         length: 2,
         fill: 'red',
         keywords: ['program'],
-        text: 'blah blah blah\nlorem ipsum dolor',
+        text: lorem,
         details: {
           theme: 'theme.environmentalProtection',
           instrument: 'instrument.category.construction',
@@ -93,13 +105,13 @@ const data = [
     status: 'status.inProgress',
     location: 'Albuquerque, NM',
     type: 'Construction of Oil Pipeline',
-    activity: 'IT IS ORDERED that, pursuant to section 58 of the act, the applied-for Project as specified in good lord this is putting me to sleep just typing it out',
+    activity: lorem,
     conditions: [
       {
         length: 3,
         fill: 'orange',
         keywords: ['program'],
-        text: 'blah blah blah\nhello lorem ipsum dolor',
+        text: lorem,
         details: {
           theme: 'theme.environmentalProtection',
           instrument: 'instrument.category.construction',
@@ -113,6 +125,12 @@ const data = [
   },
 ];
 
+const updateSelectedItem = () => (instrumentIndex, itemIndex) => (
+  { selectedItem: { instrumentIndex, itemIndex } }
+);
+
+const toggleExpanded = () => expand => ({ expanded: expand });
+
 const defaultProps = {
   data,
   selectedProject: 'Keystone XL',
@@ -122,20 +140,22 @@ const defaultProps = {
   selectedItem: { instrumentIndex: 1, itemIndex: -1 },
   openProjectDetails: project => alert(`Project details for: ${project}`),
   openIntermediatePopup: instrumentNumber => alert(`Intermediate popup for: ${instrumentNumber}`),
-  updateSelectedItem: (instrumentIndex, itemIndex) => alert(`${instrumentIndex}, ${itemIndex}`),
 };
 
 storiesForComponent('Components|ConditionDetails', module, ReadMe)
-  .addDecorator(withInteraction({ actions: ['toggleExpanded'] }))
+  .addDecorator(withInteraction({ actions: ['updateSelectedItem', 'toggleExpanded'] }))
   .add('default', () => (
-    <ConditionDetails {...defaultProps} />
-  ))
+    <ConditionDetails
+      {...defaultProps}
+      {...getInteractionProps()}
+    />
+  ), { interaction: { actions: { toggleExpanded, updateSelectedItem } } })
   .add('expandable', () => (
     <ConditionDetails
       {...defaultProps}
       selectedItem={{ instrumentIndex: 1, itemIndex: 0 }}
       isExpandable
+      expanded
       {...getInteractionProps()}
-      expanded={getInteractionProps().expanded}
     />
-  ), { interaction: { actions: { toggleExpanded: () => expand => ({ expanded: expand }) } } });
+  ), { interaction: { actions: { toggleExpanded, updateSelectedItem } } });
