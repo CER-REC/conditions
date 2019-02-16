7310c3e534eef10c157938aed635b4e3d43e86db
ccheuvizworx <30633884+ccheuvizworx@users.noreply.github.com>
2019-02-15 17:07:26 -0700

    feat(instruments bubble legend): adds min size for ellipsis and takes logic out of render
    Issues Affected: NEBV-1284

--- src/components/InstrumentsLegend/BubbleLegend/index.jsx
+++ src/components/InstrumentsLegend/BubbleLegend/index.jsx
@@ -4,29 +4,44 @@ import classNames from 'classnames';
 import { FormattedMessage } from 'react-intl';
 import './styles.scss';
 
-const BubbleLegend = props => (
-  <div className={classNames(
-    'BubbleLegend',
-    props.className,
-  )}
-  >
-    <FormattedMessage id="components.projectLegend.numberOfConditions" />
-    <svg viewBox="-200 0 260 300">
-      <ellipse className="Ellipse" cx={-(props.radiusOfMaxBubble * 10) / props.maxConditions} cy="3" rx={(props.radiusOfMaxBubble * 10) / props.maxConditions} ry="3" transform="translate(0, 23)" />
-      <text className="Text" x="30" y="30">10</text>
-      <ellipse className="Ellipse" cx={-(props.radiusOfMaxBubble * 100) / props.maxConditions} cy="10" rx={(props.radiusOfMaxBubble * 100) / props.maxConditions} ry="5" transform="translate(0, 34)" />
-      <text className="Text" x="30" y="50">100</text>
-      <ellipse className="Ellipse" cx={-(props.radiusOfMaxBubble * 1000) / props.maxConditions} cy="10" rx={(props.radiusOfMaxBubble * 1000) / props.maxConditions} ry="5" transform="translate(0, 54)" />
-      <text className="Text" x="30" y="70">1000</text>
-      <ellipse className="Ellipse" cx={-(props.radiusOfMaxBubble * props.maxConditions) / props.maxConditions} cy="10" rx={props.radiusOfMaxBubble} ry="5" transform="translate(0, 74)" />
-      <text className="Text" x="30" y="90">{props.maxConditions}</text>
-    </svg>
-  </div>
-);
+const BubbleLegend = (props) => {
+  const values = [10, 100, 1000, props.maxConditions];
+  const ellipsis = values.map((k, i) => {
+    let ellipsisRadius = (props.radiusOfMaxBubble * k) / props.maxConditions;
+    if (ellipsisRadius <= 4) { ellipsisRadius = 3; }
+    return (
+      <g key={k}>
+        <ellipse
+          className="Ellipse"
+          cx={-(props.radiusOfMaxBubble * k) / props.maxConditions}
+          cy="3"
+          rx={ellipsisRadius}
+          ry={3 + i}
+          transform={`translate(0, ${(i * 20) + 23})`}
+        />
+        <text className="Text" x="30" y={30 + (i * 20)}>{k}</text>
+      </g>
+    );
+  });
+
+  return (
+    <div className={classNames(
+      'BubbleLegend',
+      props.className,
+    )}
+    >
+      <FormattedMessage id="components.projectLegend.numberOfConditions" />
+      <svg viewBox="-200 0 260 300">
+        {ellipsis}
+      </svg>
+    </div>
+  );
+};
 
 BubbleLegend.propTypes = {
   className: PropTypes.string.isRequired,
-  maxConditions: PropTypes.number.isRequired, // number of Conditions in largest Bubble
+  /* number of Conditions in largest Bubble */
+  maxConditions: PropTypes.number.isRequired,
   radiusOfMaxBubble: PropTypes.number.isRequired,
 };
 
