7a80d855fc1efb1e3b6f2cc9d1cfba97756f3e0f
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-02-17 00:47:03 -0700

    refactor(SearchBar): changed stories code and styles
    Issues Affected: NEBV-1282

--- src/components/SearchBar/SearchContent/stories.jsx
+++ src/components/SearchBar/SearchContent/stories.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { forceReRender } from '@storybook/react';
 import withInteraction, { getInteractionProps } from 'storybook-addon-interaction';
 import { storiesForComponent } from '../../../../.storybook/utils';
 import withStatus from '../../../../.storybook/addon-status';
@@ -8,21 +7,23 @@ import ReadMe from './README.md';
 
 storiesForComponent('Components|SearchBar/SearchContent', module, ReadMe)
   .addDecorator(withStatus('functionalityUnderDevelopment'))
-  .addDecorator(withInteraction({
-    state: {
-      keywords: { include: ['pipeline1', 'pipeline2'], exclude: ['safety', 'fire'] }, mode: 'basic',
-    },
-    actions: {
-      changeSearchType: state => () => {
-        const mode = (state.mode === 'basic') ? 'advanced' : 'basic';
-        return ({ mode });
-      },
-      updateKeywords: () => words => ({ keywords: words }),
-    },
-  }))
+  .addDecorator(withInteraction({ actions: ['changeSearchType', 'updateKeywords'] }))
   .add('with interaction', () => (
     <SearchContent
       {...getInteractionProps()}
       closeTab={() => alert('clicked')}
     />
-  ));
+  ), {
+    interaction: {
+      state: {
+        keywords: { include: ['pipeline1', 'pipeline2'], exclude: ['safety', 'fire'] }, mode: 'basic',
+      },
+      actions: {
+        changeSearchType: state => () => {
+          const mode = (state.mode === 'basic') ? 'advanced' : 'basic';
+          return ({ mode });
+        },
+        updateKeywords: () => words => ({ keywords: words }),
+      },
+    },
+  });
