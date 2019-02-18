043f96b90dff2975a90af89f66ab8daf4b1e462f
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-02-17 20:43:24 -0700

    refactor(SearchBar): improved actions in stories.jsx
    Issues Affected: NEBV-1259

--- src/components/SearchBar/SuggestionWindow/stories.jsx
+++ src/components/SearchBar/SuggestionWindow/stories.jsx
@@ -8,103 +8,103 @@ import ReadMe from './README.md';
 
 storiesForComponent('Components|SearchBar/SuggestionWindow', module, ReadMe)
   .addDecorator(withStatus('functionalityUnderDevelopment'))
-  .addDecorator(withInteraction({
-    state: {
-      categories: ['all', 'wildlife & habitat', 'environment', 'engineering & structures', 'administration & filings'],
-      selectedCategory: ['wildlife & habitat'],
-      sortBy: '',
-      sortHierarchy: 'none',
-      selectedWords: [{
-        name: 'safety',
-        conditions: 1200,
-      },
-      {
-        name: 'emissions',
-        conditions: 1000,
-      }, {
-        name: 'habitat',
-        conditions: 800,
-      }],
-      suggestedKeywords: [{
-        name: 'safety',
-        conditions: 1200,
-      },
-      {
-        name: 'emissions',
-        conditions: 1000,
-      }, {
-        name: 'habitat',
-        conditions: 800,
-      },
-      {
-        name: 'construction',
-        conditions: 1000,
-      },
-      {
-        name: 'habitat',
-        conditions: 1000,
-      },
-      {
-        name: 'file',
-        conditions: 1400,
-      },
-      {
-        name: 'breeding breed',
-        conditions: 380,
-      },
-      {
-        name: 'safety',
-        conditions: 1400,
-      },
-      {
-        name: 'emissions',
-        conditions: 1800,
-      }, {
-        name: 'habitat',
-        conditions: 1400,
-      },
-      {
-        name: 'construction',
-        conditions: 1001,
-      },
-      {
-        name: 'habitat',
-        conditions: 1300,
-      },
-      {
-        name: 'file',
-        conditions: 1420,
-      },
-      {
-        name: 'breeding breed',
-        conditions: 390,
-      }],
-    },
-    actions: {
-      // NOTE: Storybook seems to have issue with re-rendering after state change
-      onClickUpdate: () => (obj) => {
-        const [updatedList, type] = obj;
-        if (type === 'category') {
-          forceReRender();
-          return ({ selectedCategory: updatedList });
-        }
-        forceReRender();
-        return ({ selectedWords: updatedList });
-      },
-      changeSort: () => (obj) => {
-        const [sort, type] = obj;
-        if (type === 'by') {
-          forceReRender();
-          return ({ sortBy: sort });
-        }
-        forceReRender();
-        return ({ sortHierarchy: sort });
-      },
-    },
-  }))
-  .add('default', () => (
+  .addDecorator(withInteraction({ actions: ['onClickUpdate', 'changeSort', 'closeTab'] }))
+  .add('with interaction', () => (
     <SuggestionWindow
       {...getInteractionProps()}
-      closeTab={() => alert('clicked')}
     />
-  ));
+  ), {
+    interaction: {
+      state: {
+        categories: ['all', 'wildlife & habitat', 'environment', 'engineering & structures', 'administration & filings'],
+        selectedCategory: ['wildlife & habitat'],
+        sortBy: '',
+        sortHierarchy: 'none',
+        selectedWords: [{
+          name: 'safety',
+          conditions: 1200,
+        },
+        {
+          name: 'emissions',
+          conditions: 1000,
+        }, {
+          name: 'habitat',
+          conditions: 800,
+        }],
+        suggestedKeywords: [{
+          name: 'safety',
+          conditions: 1200,
+        },
+        {
+          name: 'emissions',
+          conditions: 1000,
+        }, {
+          name: 'habitat',
+          conditions: 800,
+        },
+        {
+          name: 'construction',
+          conditions: 1000,
+        },
+        {
+          name: 'habitat',
+          conditions: 1000,
+        },
+        {
+          name: 'file',
+          conditions: 1400,
+        },
+        {
+          name: 'breeding breed',
+          conditions: 380,
+        },
+        {
+          name: 'safety',
+          conditions: 1400,
+        },
+        {
+          name: 'emissions',
+          conditions: 1800,
+        }, {
+          name: 'habitat',
+          conditions: 1400,
+        },
+        {
+          name: 'construction',
+          conditions: 1001,
+        },
+        {
+          name: 'habitat',
+          conditions: 1300,
+        },
+        {
+          name: 'file',
+          conditions: 1420,
+        },
+        {
+          name: 'breeding breed',
+          conditions: 390,
+        }],
+      },
+      actions: {
+        onClickUpdate: () => (obj) => {
+          const [updatedList, type] = obj;
+          if (type === 'category') {
+            forceReRender();
+            return ({ selectedCategory: updatedList });
+          }
+          forceReRender();
+          return ({ selectedWords: updatedList });
+        },
+        changeSort: () => (obj) => {
+          const [sort, type] = obj;
+          if (type === 'by') {
+            forceReRender();
+            return ({ sortBy: sort });
+          }
+          forceReRender();
+          return ({ sortHierarchy: sort });
+        },
+      },
+    },
+  });
