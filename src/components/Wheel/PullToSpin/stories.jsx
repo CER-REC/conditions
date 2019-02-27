d2b18ea891bc428c13c18f7becb8f643b8fb039d
Jorge Castano <jorge.castano@vizworx.com>
2019-02-27 13:02:03 -0700

    refactor(PullToSpin spec.js.snap): update rendering viewbox and update snapshot

--- src/components/Wheel/PullToSpin/stories.jsx
+++ src/components/Wheel/PullToSpin/stories.jsx
@@ -10,7 +10,7 @@ storiesForComponent('Components|Wheel/PullToSpin', module, ReadMe)
   .addDecorator(withStatus('functionalityUnderDevelopment'))
   .addDecorator(withInteraction({ actions: ['onClickSpin'] }))
   .add('default', () => (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="310 -5 110 80" transform="scale(0.25)">
+    <svg viewBox="300 0 150 100" width="200">
       <PullToSpin {...getInteractionProps()} />
     </svg>
   ));
