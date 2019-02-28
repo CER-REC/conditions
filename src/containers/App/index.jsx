c6805a5baa31f2390f90a0d12425d09867338444
Spenser Jones <spenser.jones@vizworx.com>
2019-02-28 16:19:56 -0700

    fix(Deploy): Very dirty hacks to get it working in the WET template

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import createStore from '../../Store';
+import ViewOne from '../ViewOne';
 import ViewTwo from '../ViewTwo';
 import ViewThree from '../ViewThree';
+import Footer from '../Footer';
+
+import { conditionCountsByYear, conditionCountsByCommodity } from '../../mockData';
+import conditionDetailsData from '../../components/ConditionDetails/testData';
 
 const store = createStore();
 
+const conditionDetailsProps = {
+  conditionCountsByYear,
+  conditionCountsByCommodity,
+  selected: {
+    feature: 'theme',
+    subFeature: '',
+  },
+  conditionDetails: {
+    isExpandable: true,
+    searchKeywords: {
+      include: ['hello'],
+    },
+    selectedProject: 'Keystone XL',
+    selectedItem: {
+      instrumentIndex: 0,
+      itemIndex: 0,
+    },
+    data: conditionDetailsData,
+  },
+};
+
 const App = () => (
   <Provider store={store}>
-    <ViewTwo />
-    <ViewThree />
+    <ViewOne />
+    {/* TODO: Deployment hacks */}
+    <div style={{ clear: 'both' }} />
+    <hr />
+    <ViewTwo {...conditionDetailsProps} />
+    <hr />
+    <ViewThree {...conditionDetailsProps} />
+    <hr />
+    <Footer />
   </Provider>
 );
 
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DomReady from 'domready'
+import { IntlProvider } from 'react-intl';
+import App from './containers/App';
+import './styles.scss';
+import i18nMessages from './i18n';
+
+DomReady(() => {
+  ReactDOM.render(
+    <IntlProvider locale="en" messages={i18nMessages.en}>
+      <div className="visualization"><App /></div>
+    </IntlProvider>,
+    document.getElementById('reactRoot'),
+  );
+});
