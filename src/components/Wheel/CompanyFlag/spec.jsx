5de2d093daa1c5f330fadfff1c8a8abb28e00df6
Adam Lovatt <adam.lovatt@vizworx.com>
2019-03-11 14:49:01 -0600

    feat(CompanyFlag): adds Filtered/Relevant coloring

---
+++ src/components/Wheel/CompanyFlag/spec.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CompanyFlag from '.';
+
+describe('Components|Wheel/CompanyFlag', () => {
+  describe('with default props', () => {
+    const wrapper = shallow(
+      <CompanyFlag
+        flagLayout={['1551111', '313', '77', '1']}
+      />,
+    );
+
+  });
+});
--- src/components/Wheel/CompanyFlag/stories.jsx
+++ src/components/Wheel/CompanyFlag/stories.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { storiesForComponent } from '../../../../.storybook/utils';
 import CompanyFlag from '.';
+// import ReadMe from './README.md';
+const ReadMe = '';
+
+/*
+ * 1 = Dot
+ * 3 = Filtered
+ * 5 = Relevant
+ * 7 = Filtered + Relevant
+ */
 
 const flagLayouts = [
-  ['1111111', '111', '11', '1'],
-  ['1111111', '101', '11', '1'],
-  ['1111111111111'],
+  ['1551111', '313', '77', '1'],
+  ['1551111', '303', '77', '1'],
+  ['1551111313771'],
 ];
 
-storiesForComponent('Components|Wheel/CompanyFlag')
+storiesForComponent('Components|Wheel/CompanyFlag', module, ReadMe)
   .add('full', () => (
     <svg height={400} width={200}>
       <CompanyFlag flagLayout={flagLayouts[0]} dotWidth={16} dotSpacing={24} />
