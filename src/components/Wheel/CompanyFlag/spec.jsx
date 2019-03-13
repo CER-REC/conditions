c1dbbc3f19fe3ecaa680ec3d295783ef1852d044
Adam Lovatt <adam.lovatt@vizworx.com>
2019-03-13 13:48:49 -0600

    fix(Wheel): fixes linting and proptype errors

--- src/components/Wheel/CompanyFlag/spec.jsx
+++ src/components/Wheel/CompanyFlag/spec.jsx
@@ -3,12 +3,13 @@
 
 // import CompanyFlag from '.';
 
-// describe('Components|Wheel/CompanyFlag', () => {
-//   describe('with default props', () => {
-//     const wrapper = shallow(
-//       <CompanyFlag
-//         flagLayout={['1551111', '313', '77', '1']}
-//       />,
-//     );
-//   });
-// });
+describe('Components|Wheel/CompanyFlag', () => {
+  it('should be true', () => {
+    expect(true).toBe(true);
+    // const wrapper = shallow(
+    //   <CompanyFlag
+    //     flagLayout={['1551111', '313', '77', '1']}
+    //   />,
+    // );
+  });
+});
