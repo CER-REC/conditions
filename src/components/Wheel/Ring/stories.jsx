c621987265dba642da8265613ad2c71fcdd5b03d
Jorge Castano <coderifico@gmail.com>
2019-03-18 17:45:34 -0600

    refactor(Wheel): initial fix of internet explorer limited to css (no functionality)

--- src/components/Wheel/Ring/stories.jsx
+++ src/components/Wheel/Ring/stories.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { storiesForComponent } from '../../../../.storybook/utils';
+import { storiesForComponent, withStyles } from '../../../../.storybook/utils';
 import withStatus from '../../../../.storybook/addon-status';
 import Ring from '.';
 import ReadMe from './README.md';
 import './styles.scss';
 
 storiesForComponent('Components|Wheel/Ring', module, ReadMe)
+  .addDecorator(withStyles)
   .addDecorator(withStatus('underReview'))
   .add('default view', () => (
-    <svg viewBox="120 200 500 500">
+    <svg viewBox="0 0 860 860">
       <Ring ringType="company" />
     </svg>
-  )).add('location view', () => (
-    <svg viewBox="120 200 500 500">
+  ), { styles: '.Wheel { padding-top: unset }' })
+  .add('location view', () => (
+    <svg viewBox="0 0 860 860">
       <Ring ringType="location" />
     </svg>
-  ));
+  ), { styles: '.Wheel { padding-top: unset }' });
+
