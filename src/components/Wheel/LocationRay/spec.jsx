0b724f722068242fcf0112b80a8f6b36fdfd1cce
Jorge Castano <jorge.castano@vizworx.com>
2019-03-18 10:04:36 -0600

    refactor(WheelRay): fix unnecessary array creation on story

--- src/components/Wheel/LocationRay/spec.jsx
+++ src/components/Wheel/LocationRay/spec.jsx
@@ -7,10 +7,10 @@ import LocationRay from '.';
 import { features } from '../../../constants';
 
 const values = [1, 3, 5, 2, 9, 15, 6, 8, 5, 2, 9, 15, 6, 8];
-const randomLocationBars = [Object.keys(features.theme).map((subFeature, index) => ({
+const randomLocationBars = Object.keys(features.theme).map((subFeature, index) => ({
   value: values[index],
   fill: features.theme[subFeature],
-}))];
+}));
 
 const degreesPerItem = 5;
 
@@ -18,7 +18,7 @@ describe('Components|Wheel/LocationRay', () => {
   describe('with default props', () => {
     const wrapper = shallow(
       <LocationRay
-        items={randomLocationBars[0]}
+        items={randomLocationBars}
         height={degreesPerItem * 2}
         width="163"
         searched
