ff302860c4efd4a33776d2cc48e4ddd2e9976d43
Spenser Jones <hello@spenserj.com>
2019-03-20 10:17:58 -0600

    refactor(ProjectMenu): move spec data into spec and change project IDs
    Using project IDs that start at 100 will provide an easier visual comparison between the array index
    and the project ID

--- src/components/ProjectMenu/spec.jsx
+++ src/components/ProjectMenu/spec.jsx
@@ -1,16 +1,122 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ProjectMenu from '.';
-import * as testData from './specMockData';
+
+const testData = [
+  {
+    id: 100,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 101,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 102,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 103,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 104,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 105,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 106,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+  {
+    id: 107,
+    name: { en: 'Trans-Alta Limited-44245', fr: '' },
+    shortName: { en: '', fr: '' },
+    data: {
+      theme: { ADMINISTRATIVE: 10, DAMAGE_PREVENTION: 0 },
+      instrument: {},
+      phase: {},
+      status: {},
+      type: {},
+      filing: {},
+    },
+  },
+];
 
 describe('Components|ProjectMenu', () => {
-  const selectedProjectID = 1228;
+  const selectedProjectID = 105;
   const noop = () => {};
   describe('with default props', () => {
     let wrapper;
     beforeEach(() => {
       wrapper = shallow(<ProjectMenu
-        projectsData={testData.data}
+        projectsData={testData}
         selectedProjectID={selectedProjectID}
         onChange={noop}
         selectedFeature="theme"
@@ -55,50 +161,50 @@ describe('Components|ProjectMenu', () => {
   // Outer key is the number of items in the source list
   // Inner key is the menu index that is selected
   //
-  // 8: { 0: { listIndex: 0, before: 2, items: [1223, 1224, 1225], after: 0 } }
+  // 8: { 0: { listIndex: 0, before: 2, items: [100, 101, 102], after: 0 } }
   // with 8 items
   //   and project at index 0 selected
   //     should show 3 projects
   //     should have padding of 2 before and 0 after
   //     should pass the List a selected index of 0
-  //     clicking list index 0 should emit project ID 1223
-  //     clicking list index 1 should emit project ID 1224
-  //     clicking list index 2 should emit project ID 1225
+  //     clicking list index 0 should emit project ID 100
+  //     clicking list index 1 should emit project ID 101
+  //     clicking list index 2 should emit project ID 102
   const groupedTestCases = {
     8: {
-      0: { listIndex: 0, before: 2, items: [1223, 1224, 1225], after: 0 },
-      1: { listIndex: 1, before: 1, items: [1223, 1224, 1225, 1226], after: 0 },
-      2: { listIndex: 2, before: 0, items: [1223, 1224, 1225, 1226, 1227], after: 0 },
-      3: { listIndex: 2, before: 0, items: [1224, 1225, 1226, 1227, 1228], after: 0 },
-      4: { listIndex: 2, before: 0, items: [1225, 1226, 1227, 1228, 1229], after: 0 },
-      5: { listIndex: 2, before: 0, items: [1226, 1227, 1228, 1229, 1230], after: 0 },
-      6: { listIndex: 2, before: 0, items: [1227, 1228, 1229, 1230], after: 1 },
-      7: { listIndex: 2, before: 0, items: [1228, 1229, 1230], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100, 101, 102], after: 0 },
+      1: { listIndex: 1, before: 1, items: [100, 101, 102, 103], after: 0 },
+      2: { listIndex: 2, before: 0, items: [100, 101, 102, 103, 104], after: 0 },
+      3: { listIndex: 2, before: 0, items: [101, 102, 103, 104, 105], after: 0 },
+      4: { listIndex: 2, before: 0, items: [102, 103, 104, 105, 106], after: 0 },
+      5: { listIndex: 2, before: 0, items: [103, 104, 105, 106, 107], after: 0 },
+      6: { listIndex: 2, before: 0, items: [104, 105, 106, 107], after: 1 },
+      7: { listIndex: 2, before: 0, items: [105, 106, 107], after: 2 },
     },
     5: {
-      0: { listIndex: 0, before: 2, items: [1223, 1224, 1225], after: 0 },
-      1: { listIndex: 1, before: 1, items: [1223, 1224, 1225, 1226], after: 0 },
-      2: { listIndex: 2, before: 0, items: [1223, 1224, 1225, 1226, 1227], after: 0 },
-      3: { listIndex: 2, before: 0, items: [1224, 1225, 1226, 1227], after: 1 },
-      4: { listIndex: 2, before: 0, items: [1225, 1226, 1227], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100, 101, 102], after: 0 },
+      1: { listIndex: 1, before: 1, items: [100, 101, 102, 103], after: 0 },
+      2: { listIndex: 2, before: 0, items: [100, 101, 102, 103, 104], after: 0 },
+      3: { listIndex: 2, before: 0, items: [101, 102, 103, 104], after: 1 },
+      4: { listIndex: 2, before: 0, items: [102, 103, 104], after: 2 },
     },
     4: {
-      0: { listIndex: 0, before: 2, items: [1223, 1224, 1225], after: 0 },
-      1: { listIndex: 1, before: 1, items: [1223, 1224, 1225, 1226], after: 0 },
-      2: { listIndex: 2, before: 0, items: [1223, 1224, 1225, 1226], after: 1 },
-      3: { listIndex: 2, before: 0, items: [1224, 1225, 1226], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100, 101, 102], after: 0 },
+      1: { listIndex: 1, before: 1, items: [100, 101, 102, 103], after: 0 },
+      2: { listIndex: 2, before: 0, items: [100, 101, 102, 103], after: 1 },
+      3: { listIndex: 2, before: 0, items: [101, 102, 103], after: 2 },
     },
     3: {
-      0: { listIndex: 0, before: 2, items: [1223, 1224, 1225], after: 0 },
-      1: { listIndex: 1, before: 1, items: [1223, 1224, 1225], after: 1 },
-      2: { listIndex: 2, before: 0, items: [1223, 1224, 1225], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100, 101, 102], after: 0 },
+      1: { listIndex: 1, before: 1, items: [100, 101, 102], after: 1 },
+      2: { listIndex: 2, before: 0, items: [100, 101, 102], after: 2 },
     },
     2: {
-      0: { listIndex: 0, before: 2, items: [1223, 1224], after: 1 },
-      1: { listIndex: 1, before: 1, items: [1223, 1224], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100, 101], after: 1 },
+      1: { listIndex: 1, before: 1, items: [100, 101], after: 2 },
     },
     1: {
-      0: { listIndex: 0, before: 2, items: [1223], after: 2 },
+      0: { listIndex: 0, before: 2, items: [100], after: 2 },
     },
   };
 
@@ -113,8 +219,8 @@ describe('Components|ProjectMenu', () => {
           onChange = jest.fn();
           wrapper = shallow(
             <ProjectMenu
-              projectsData={testData.data.slice(0, inputTotal)}
-              selectedProjectID={testData.data[menuIndex].id}
+              projectsData={testData.slice(0, inputTotal)}
+              selectedProjectID={testData[menuIndex].id}
               onChange={onChange}
               selectedFeature="theme"
             />,
