f1a4a493a531c04f3b0b496e9018eb6af2314709
Adam Lovatt <adam.lovatt@vizworx.com>
2019-04-08 14:44:18 -0600

    feat(App): uses translate for improved performance when moving the Guide

--- src/components/Guide/index.jsx
+++ src/components/Guide/index.jsx
@@ -6,9 +6,16 @@ import CircleContainer from '../CircleContainer';
 import './styles.scss';
 
 const Guide = ({ textState }) => (
-  <CircleContainer className="Guide" size={128}>
-    <FormattedMessage id={`components.guide.tutorial.${(textState < 9) ? textState : 0}`} />
-  </CircleContainer>
+  /**
+   * This wrapper div gets us around the fact that CSS' translate function measures
+   * percentages relative to the element being translated; the Guide circle itself
+   * can't use percentages for translating to a given position relative to the app.
+   */
+  <div className="Guide">
+    <CircleContainer size={128}>
+      <FormattedMessage id={`components.guide.tutorial.${(textState < 9) ? textState : 0}`} />
+    </CircleContainer>
+  </div>
 );
 
 Guide.propTypes = {
