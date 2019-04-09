e65505b60c88680b6c33ae2768df611744837f62
Adam Lovatt <adam.lovatt@vizworx.com>
2019-04-09 09:50:04 -0600

    feat(BrowseBy): adds missing callback prop

--- src/components/BrowseBy/index.jsx
+++ src/components/BrowseBy/index.jsx
@@ -7,23 +7,22 @@ import BrowseByBtn from './BrowseByBtn';
 
 import './styles.scss';
 
-const noop = () => {};
-
-const BrowseBy = ({ showArrow, labelId }) => (
+const BrowseBy = ({ showArrow, labelId, setBrowseBy }) => (
   <div className="BrowseBy">
     <div className={classNames('arrowWrapper', { showArrow, zeroWidth: (labelId === 'blank') })}>
       <FormattedMessage id={`components.browseBy.${labelId}`}>
         {text => <span className="label">{text}</span>}
       </FormattedMessage>
     </div>
-    <BrowseByBtn classNames="company" mode="company" onClick={noop} />
-    <BrowseByBtn classNames="location" mode="location" onClick={noop} />
+    <BrowseByBtn classNames="company" mode="company" onClick={setBrowseBy} />
+    <BrowseByBtn classNames="location" mode="location" onClick={setBrowseBy} />
   </div>
 );
 
 BrowseBy.propTypes = {
   showArrow: PropTypes.bool,
   labelId: PropTypes.oneOf(['skip', 'return', 'blank']),
+  setBrowseBy: PropTypes.func.isRequired,
 };
 
 BrowseBy.defaultProps = {
