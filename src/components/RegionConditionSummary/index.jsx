27d3e96e204b5555d7617d2592452598211a539c
Adam Lovatt <adam.lovatt@vizworx.com>
2019-04-24 16:15:21 -0600

    feat(View 2): uses variables for color styles

--- src/components/RegionConditionSummary/index.jsx
+++ src/components/RegionConditionSummary/index.jsx
@@ -27,10 +27,10 @@ const RegionConditionSummary = (props) => {
         </div>
       </div>
       <svg className="YAxis" viewBox="0 0 100 200">
-        <text x="45" y="15" fill="rgb(161, 168, 167)">100</text>
-        <rect x="85" y="5" width="10" height="3" fill="#e4e4e4" />
-        <text x="58" y="100" fill="rgb(161, 168, 167)">0</text>
-        <rect x="85" y="50" width="10" height="3" fill="#e4e4e4" />
+        <text x="45" y="15">100</text>
+        <rect x="85" y="5" width="10" height="3" />
+        <text x="58" y="100">0</text>
+        <rect x="85" y="50" width="10" height="3" />
       </svg>
     </div>
   );
