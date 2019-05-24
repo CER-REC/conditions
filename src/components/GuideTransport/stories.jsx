ec6309c839f714bdd2f4a8bb868427c014cdc29c
Adam Lovatt <adam.lovatt@vizworx.com>
2019-05-24 11:53:26 -0600

    feat(GuideTransport): adds SVGs and caption

--- src/components/GuideTransport/stories.jsx
+++ src/components/GuideTransport/stories.jsx
@@ -3,7 +3,12 @@ import { storiesForComponent } from '../../../.storybook/utils';
 import GuideTransport from '.';
 import ReadMe from './README.md';
 
+const noop = () => {};
+
 storiesForComponent('Components|GuideTransport', module, ReadMe)
   .add('default', () => (
-    <GuideTransport />
+    <GuideTransport back={noop} forward={noop} togglePlay={noop} />
+  ))
+  .add('playing', () => (
+    <GuideTransport playing back={noop} forward={noop} togglePlay={noop} />
   ));
