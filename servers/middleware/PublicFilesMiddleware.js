9dbb527b4bff8521f6f59cd5afe5ae21e8d9ea4a
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2019-05-27 11:45:04 -0600

    removed debuggin statement

--- servers/middleware/PublicFilesMiddleware.js
+++ servers/middleware/PublicFilesMiddleware.js
@@ -6,7 +6,6 @@ const PublicFilesMiddleware = () => {
   const app = Express();
   app.use(Express.static(Path.join(ApplicationRoot, 'public')));
   app.use(Express.static(Path.join(ApplicationRoot, 'devPublic')));
-  app.use('/data', Express.static(Path.join(ApplicationRoot, 'data')));
   return app;
 };
 module.exports = PublicFilesMiddleware;
