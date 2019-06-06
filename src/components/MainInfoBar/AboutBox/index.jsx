c90ede1610f9500c9e23024a17817cf6c248af07
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2019-06-06 09:20:13 -0600

    refactor(GraphQL): added links, updated snapshots

--- src/components/MainInfoBar/AboutBox/index.jsx
+++ src/components/MainInfoBar/AboutBox/index.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+const formatLink = (link, text) => (
+  <a href={link} rel="noopener noreferrer" target="_blank">
+    <FormattedMessage id={text} />
+  </a>
+);
+
 const AboutBox = () => (
   <div className="AboutBox">
     <FormattedMessage id="components.mainInfoBar.headings.about" tagName="h1" />
+    <FormattedMessage
+      id="components.mainInfoBar.content.about"
+      values={{
+        nebLink: formatLink('mailto:energyindesign@neb-one.gc.ca.', 'common.linkText.NEBLink'),
+      }}
+    />
     <FormattedMessage id="components.mainInfoBar.content.about">
       {/* eslint-disable-next-line react/no-array-index-key */}
       {text => text.split('\n').map((para, idx) => <p key={idx}>{para}</p>)}
     </FormattedMessage>
+    {/* TODO: Fix the styling for contributors */}
     <FormattedMessage id="components.mainInfoBar.headings.contributor" tagName="h1" />
     <FormattedMessage id="components.mainInfoBar.content.contributor">
       {/* eslint-disable-next-line react/no-array-index-key */}
