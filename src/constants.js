c0be995cf627dbc9c1e3c6e665ff337adc8b8ec6
Spenser Jones <hello@spenserj.com>
2019-06-07 13:01:08 -0600

    feat(GraphQL): adds lang code to graphql endpoint
    This adds support for server-side translation of various strings. In storybook,
    the language can be changed by running `window.localStorage.setItem('dev-lang', 'fr')`
    
    Issues Affected: NEBV-1468

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,18 @@
+export const applicationPath = {
+  en: 'conditions',
+  fr: 'conditions-fr',
+};
+
+export const lang = (typeof document !== 'undefined'
+    && document.location
+    && document.location.href
+    && document.location.href.includes(applicationPath.fr))
+  || (process.env.NODE_ENV === 'development'
+    && typeof window !== 'undefined'
+    && window.localStorage
+    && window.localStorage.getItem('dev-lang') === 'fr')
+  ? 'fr' : 'en';
+
 export const features = {
   instrument: {
     0: 'rgb(122, 25, 17)',
