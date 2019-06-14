da73bb0e938f31a860781307872c8b92516c664d
Rahul Kamal Bhaskar <rahul.kamal.bhaskar@vizworx.com>
2019-06-14 08:16:13 -0600

    updated the translation table to constant file

--- src/RouteComputations.js
+++ src/RouteComputations.js
@@ -1,15 +1,14 @@
-import { appHost } from './constants';
-import Tr from './TranslationTable';
+import { appHost, applicationPath } from './constants';
 
 const RouteComputations = {
   bitlyParameter(language) {
-    return `${appHost}/${Tr.getIn(['applicationPath', language])}/${encodeURIComponent(document.location.search)}`;
+    return `${appHost}/${applicationPath[language]}/${encodeURIComponent(document.location.search)}`;
   },
   bitlyEndpoint() {
     return `${document.location.origin}/bitlyService/api/bitlyShortlink`;
   },
   applicationPath(language) {
-    return Tr.getIn(['applicationPath', language]);
+    return applicationPath[language];
   },
   appRoot(language) {
     return `${document.location.origin}/${RouteComputations.applicationPath(language)}`;
