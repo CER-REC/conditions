7a301a0a4a62a4a00da3d38352ec0d15a129e9a1
Adam Lovatt <adam.lovatt@vizworx.com>
2019-06-18 10:40:31 -0600

    fix(ConditionExplorer): directly sets the outline position to avoid jittering

--- src/components/ConditionExplorer/PhysicsVariant/Body.js
+++ src/components/ConditionExplorer/PhysicsVariant/Body.js
@@ -114,11 +114,16 @@ export default class Body {
       y: start.y + ((end.y - start.y) * inOut),
     };
 
-    // Assuming velocity is measured in units-per-frame here
-    Matter.Body.setVelocity(this.body, {
-      x: next.x - this.body.position.x,
-      y: next.y - this.body.position.y,
-    });
+    // Even though we're directly setting the position, the Outline still needs
+    // to have a velocity set for it to collide properly with keywords
+    if (this.constructor.name === 'Outline') {
+      Matter.Body.setVelocity(this.body, {
+        x: (next.x - this.body.position.x),
+        y: (next.y - this.body.position.y),
+      });
+    }
+
+    Matter.Body.setPosition(this.body, next);
   }
 
   onUpdateRotation(inOut, start, end) {
