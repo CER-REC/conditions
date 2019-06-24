f18f2378aa6b7429c7ef4d304f0e2021568b15e0
Adam Lovatt <adam.lovatt@vizworx.com>
2019-06-24 17:32:36 -0600

    feat(App): adds queries and processing for trees by Project and Company, adds region IDs

---
+++ src/queries/getTreeFromCompany.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  query getTreeFromCompany($id: Int!) {
+    getCompanyById(id: $id){
+      projects {
+        id
+        instruments {
+          id
+          regionIds
+        }
+      }
+    }
+  }
+`;
