756d00006195e73376bd69da8b9752fa8c18fded
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-05 10:28:51 -0600

    feat(SearchBar): adds highlighting to keywords in the search summary

--- src/components/SearchBar/HighlightSummary/index.jsx
+++ src/components/SearchBar/HighlightSummary/index.jsx
@@ -4,15 +4,29 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import { yearRangeType } from '../../../proptypes';
 
+const joinJsxArray = (arr, joinWith) => {
+  if (!arr || arr.length < 2) { return arr; }
+
+  const out = [arr[0]];
+  for (let i = 1; i < arr.length; i += 1) {
+    out.push(joinWith, arr[i]);
+  }
+  return out;
+};
+
+const formatSummaryText = (arr, mapFunc) => joinJsxArray(arr.map(mapFunc), ', ');
+
 const HighlightSummary = props => (
   <div className="HighlightSummary">
     <FormattedMessage id="components.searchBar.highlightSummary.showingAll" />
     <span> {props.selectedYear.start} - {props.selectedYear.end } </span>
     <br />
     <FormattedMessage id="components.searchBar.highlightSummary.includingStatuses" />
-    <div> {props.includedStatuses.map(
-      status => <FormattedMessage key={status} id={`components.searchBar.filter.projectStatus.${status}`} />,
-    ).reduce((prev, curr) => [prev, ', ', curr])}
+    <div>
+      {formatSummaryText(
+        props.includedStatuses,
+        status => <FormattedMessage key={status} id={`components.searchBar.filter.projectStatus.${status}`} />,
+      )}
     </div>
     {props.includeKeywords.length === 0 ? null : (
       <React.Fragment>
@@ -20,7 +34,10 @@ const HighlightSummary = props => (
           {text => (<p>{text} : </p>)}
         </FormattedMessage>
         <div className="keywordsText">
-          {props.includeKeywords.join(', ')}
+          {formatSummaryText(
+            props.includeKeywords,
+            keyword => <span key={keyword}>{keyword}</span>,
+          )}
         </div>
       </React.Fragment>
     )}
@@ -30,7 +47,10 @@ const HighlightSummary = props => (
           {text => (<p>{text} : </p>)}
         </FormattedMessage>
         <div className="keywordsText">
-          {props.excludeKeywords.join(', ')}
+          {formatSummaryText(
+            props.excludeKeywords,
+            keyword => <span key={keyword}>{keyword}</span>,
+          )}
         </div>
       </React.Fragment>
     )}
