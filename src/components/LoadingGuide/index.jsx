11cfba78c1f2f298bf6fe4997f3ad7d2d04bf4d5
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-08 12:10:04 -0600

    refactor(LoadingGuide): sets the timer using appropriate lifecycle methods

--- src/components/LoadingGuide/index.jsx
+++ src/components/LoadingGuide/index.jsx
@@ -10,9 +10,15 @@ class LoadingGuide extends React.PureComponent {
     this.state = { step: 0 };
   }
 
+  componentDidMount() { this.setTimeout(); }
+
+  componentDidUpdate() { this.setTimeout(); }
+
   componentWillUnmount() {
     if (this.timeout) { clearTimeout(this.timeout); }
   }
+
+  setTimeout = () => { this.timeout = setTimeout(this.incrementStep, 500); }
 
   incrementStep = () => {
     this.setState(prevState => ({ step: prevState.step + 1 }));
@@ -20,7 +26,6 @@ class LoadingGuide extends React.PureComponent {
 
   render() {
     const guideStep = (this.state.step % 7) + 1;
-    this.timeout = setTimeout(this.incrementStep, 500);
     return <Guide className="LoadingGuide" loading step={guideStep} onClick={noop} />;
   }
 }
