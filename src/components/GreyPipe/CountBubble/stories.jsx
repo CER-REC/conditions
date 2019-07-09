d9e46acb0672d2a357b7f19664b6e827e0695a18
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-09 16:32:20 -0600

    feat(GreyPipe): adds stories for edge cases

--- src/components/GreyPipe/CountBubble/stories.jsx
+++ src/components/GreyPipe/CountBubble/stories.jsx
@@ -4,4 +4,6 @@ import CountBubble from '.';
 import ReadMe from './README.md';
 
 storiesForComponent('Components|GreyPipe/CountBubble', module, ReadMe)
-  .add('default', () => <CountBubble count={42} textId="projects" />);
+  .add('default', () => <CountBubble count={42} textId="projects" />)
+  .add('singular', () => <CountBubble count={1} textId="conditions" />)
+  .add('large value', () => <CountBubble count={1723} textId="instruments" />);
