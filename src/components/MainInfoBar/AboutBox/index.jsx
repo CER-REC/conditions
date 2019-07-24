3dcbdcd0806c22a784427da282eaeb453fa18081
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-24 13:38:21 -0600

    fix(MainInfoBar): fixes duplicate text, updates styling to match design doc

--- src/components/MainInfoBar/AboutBox/index.jsx
+++ src/components/MainInfoBar/AboutBox/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+const nebEmailUrl = 'mailto:energyindesign@neb-one.gc.ca';
+
 const formatLink = (link, text) => (
   <a href={link} rel="noopener noreferrer" target="_blank">
     <FormattedMessage id={text} />
@@ -10,22 +12,14 @@ const formatLink = (link, text) => (
 const AboutBox = () => (
   <div className="AboutBox">
     <FormattedMessage id="components.mainInfoBar.headings.about" tagName="h1" />
-    <FormattedMessage
-      id="components.mainInfoBar.content.about"
-      values={{
-        nebLink: formatLink('mailto:energyindesign@neb-one.gc.ca.', 'common.linkText.NEBLink'),
-      }}
-    />
-    <FormattedMessage
-      id="components.mainInfoBar.content.about"
-      values={{
-        nebLink: formatLink('mailto:energyindesign@neb-one.gc.ca.', 'common.linkText.NEBLink'),
-      }}
-    >
+    <FormattedMessage id="components.mainInfoBar.content.about">
       {/* eslint-disable-next-line react/no-array-index-key */}
       {text => text.split('\n').map((para, idx) => <p key={idx}>{para}</p>)}
     </FormattedMessage>
-    {/* TODO: Fix the styling for contributors */}
+    <FormattedMessage id="components.mainInfoBar.content.emailUs">
+      {text => <p key="emailUs">{text}: {formatLink(nebEmailUrl, 'common.linkText.NEBLink')}</p>}
+    </FormattedMessage>
+    <FormattedMessage id="components.mainInfoBar.content.lookForward" />
     <FormattedMessage id="components.mainInfoBar.headings.contributor" tagName="h1" />
     <FormattedMessage id="components.mainInfoBar.content.contributor">
       {/* eslint-disable-next-line react/no-array-index-key */}
