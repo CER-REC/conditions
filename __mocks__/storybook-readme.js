3b80e4e214d1134e9a1cba9ad995d2c472f582d1
Spenser Jones <hello@spenserj.com>
2019-07-26 09:00:39 -0600

    test(storybook-readme): fixes mock for storybook-readme to work with v5.0.5
    The mock is used to prevent the readmes from showing up in the snapshots

--- __mocks__/storybook-readme.js
+++ __mocks__/storybook-readme.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const doc = () => () => React.createElement('div');
-export const withDocs = () => (storyFn, context) => storyFn(context);
-export const withReadme = () => (storyFn, context) => storyFn(context);
+export const addReadme = (storyFn, context) => storyFn(context);
+export const configureReadme = () => () => {};
