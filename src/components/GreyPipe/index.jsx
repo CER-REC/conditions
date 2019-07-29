be28d831a966a879431456bbf96dd45bbbbebb70
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-29 16:50:25 -0600

    feat(App): adjusts layout and styling of ConditionDetails to maximize space

--- src/components/GreyPipe/index.jsx
+++ src/components/GreyPipe/index.jsx
@@ -29,7 +29,6 @@ const GreyPipe = ({ mode, conditionCount, instrumentCount, projectCount }) => (
       }
     </div>
     <div className="bottom" />
-    {mode === 'location' ? <div className="bottomConnection" /> : null}
   </div>
 );
 
