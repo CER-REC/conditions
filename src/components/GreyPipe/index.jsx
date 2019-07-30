4a20f8f16205252d849d6e7113b729785594daf6
Adam Lovatt <adam.lovatt@vizworx.com>
2019-07-30 10:39:54 -0600

    feat(GreyPipe): tweaks placement of count bubbles as per new design

--- src/components/GreyPipe/index.jsx
+++ src/components/GreyPipe/index.jsx
@@ -16,7 +16,7 @@ const GreyPipe = ({ mode, conditionCount, instrumentCount, projectCount }) => (
       )
     }
     <div className="top" />
-    <div className="vertical">
+    <div className={classNames('vertical', { withBubbles: conditionCount !== null })}>
       {(mode === 'company' && conditionCount !== null)
         ? (
           <React.Fragment>
