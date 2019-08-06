e9f7b16bc37478815fd248cbf2dc4311ac1e05b0
Kaitlin de Chastelain Finnigan <kaitlin.decf@gmail.com>
2019-08-06 14:54:01 -0600

    feat(Methodology): adds Keyword Methodology
    Issues Affected: NEBV-1667

--- src/components/MainInfoBar/MethodologyBox/index.jsx
+++ src/components/MainInfoBar/MethodologyBox/index.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import AdvancedFormattedMessage from '../../AdvancedFormattedMessage';
 import { lang } from '../../../constants';
+import methodologyKeywordEN1 from '../../../../public/images/KeywordMethodology1_EN_480w.png';
+import methodologyKeywordEN2 from '../../../../public/images/KeywordMethodology2_EN_480w.png';
+import methodologyKeywordEN3 from '../../../../public/images/KeywordMethodology3_EN_480w.png';
+import methodologyKeywordEN4 from '../../../../public/images/KeywordMethodology4_EN_480w.png';
+import methodologyKeywordEN5 from '../../../../public/images/KeywordMethodology5_EN_480w.png';
+import methodologyKeywordEN6 from '../../../../public/images/KeywordMethodology6_EN_480w.png';
+import methodologyKeywordFR1 from '../../../../public/images/KeywordMethodology1_FR_480w.png';
+import methodologyKeywordFR2 from '../../../../public/images/KeywordMethodology2_FR_480w.png';
+import methodologyKeywordFR3 from '../../../../public/images/KeywordMethodology3_FR_480w.png';
+import methodologyKeywordFR4 from '../../../../public/images/KeywordMethodology4_FR_480w.png';
+import methodologyKeywordFR5 from '../../../../public/images/KeywordMethodology5_FR_480w.png';
+import methodologyKeywordFR6 from '../../../../public/images/KeywordMethodology6_FR_480w.png';
+import './styles.scss';
 
 const MethodologyBox = () => (
   <div className="MethodologyBox">
@@ -22,26 +35,25 @@ const MethodologyBox = () => (
       }}
     />
     <FormattedMessage id="components.mainInfoBar.headings.keywords" tagName="h1" />
-    <FormattedMessage id="components.mainInfoBar.content.keywords" tagName="p" />
     {lang === 'en'
       ? (
-        <div>
-          <img src="KeywordMethodology1_EN_480w.png" alt="English1" /><br />
-          <img src="../../../images/KeywordMethodology2_EN_480w.png" alt="English2" /><br />
-          <img src="../../../images/KeywordMethodology3_EN_480w.png" alt="English3" /><br />
-          <img src="../../../images/KeywordMethodology4_EN_480w.png" alt="English4" /><br />
-          <img src="../../../images/KeywordMethodology5_EN_480w.png" alt="English5" /><br />
-          <img src="../../../images/KeywordMethodology6_EN_480w.png" alt="English6" /><br />
+        <div className="KeywordExplanation">
+          <img src={methodologyKeywordEN1} alt="KeywordMethodologyEN1" />
+          <img src={methodologyKeywordEN2} alt="KeywordMethodologyEN2" />
+          <img src={methodologyKeywordEN3} alt="KeywordMethodologyEN3" />
+          <img src={methodologyKeywordEN4} alt="KeywordMethodologyEN4" />
+          <img src={methodologyKeywordEN5} alt="KeywordMethodologyEN5" />
+          <img src={methodologyKeywordEN6} alt="KeywordMethodologyEN6" />
         </div>
       )
       : (
-        <div>
-          <img src="../../../images/KeywordMethodology1_FR_480w.png" alt="French1" /><br />
-          <img src="../../../images/KeywordMethodology2_FR_480w.png" alt="French2" /><br />
-          <img src="../../../images/KeywordMethodology3_FR_480w.png" alt="French3" /><br />
-          <img src="../../../images/KeywordMethodology4_FR_480w.png" alt="French4" /><br />
-          <img src="../../../images/KeywordMethodology5_FR_480w.png" alt="French5" /><br />
-          <img src="../../../images/KeywordMethodology6_FR_480w.png" alt="French6" /><br />
+        <div className="KeywordExplanation">
+          <img src={methodologyKeywordFR1} alt="KeywordMethodologyFR1" />
+          <img src={methodologyKeywordFR2} alt="KeywordMethodologyFR2" />
+          <img src={methodologyKeywordFR3} alt="KeywordMethodologyFR3" />
+          <img src={methodologyKeywordFR4} alt="KeywordMethodologyFR4" />
+          <img src={methodologyKeywordFR5} alt="KeywordMethodologyFR5" />
+          <img src={methodologyKeywordFR6} alt="KeywordMethodologyFR6" />
         </div>
       )
     }
