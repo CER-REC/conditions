9c80d6a6eb2ac3659efabfd0ca4d727072bde1e3
Adam Lovatt <adam.lovatt@vizworx.com>
2019-09-10 09:49:33 -0600

    feat(App): memoizes the analytics interaction wrapper

--- src/utilities/analyticsReporting.js
+++ src/utilities/analyticsReporting.js
@@ -1,5 +1,7 @@
+import memoize from 'lodash.memoize';
 import { lang, transitionStates } from '../constants';
 import handleInteraction from './handleInteraction';
+import memoizeReference from './memoizeReference';
 
 const staticDetails = {
   visualization: 'conditions',
@@ -59,12 +61,30 @@ export const reportAnalytics = (action, category, label) => {
   // return window.dataLayer.push(dataObject);
 };
 
-export const handleAnalyticsInteraction = (
+/**
+ * Usage is pretty much the same as handleInteraction, with two additional fields
+ * in front for the event category and label (see descriptions above)
+ *
+ * Example:
+ * <div
+ *   {...handleAnalyticsInteraction('browseBy', `to ${props.mode}`, props.onClick, props.mode)}
+ * />
+ */
+export const handleAnalyticsInteraction = memoize((
   category,
   label,
   callback,
   ...boundArgs
 ) => handleInteraction((e) => {
   reportAnalytics(e.type, category, label);
+
   return callback(...boundArgs, e);
+}), (category, label, callback, ...boundArgs) => {
+  const argIds = boundArgs.map(v => (
+    (typeof v === 'function' || typeof v === 'object')
+      ? `REF${memoizeReference(v)}`
+      : v
+  )).join('-');
+
+  return `${memoizeReference(callback)}-${category}-${label}-${argIds}`;
 });
