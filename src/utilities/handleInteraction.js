9bdd22cdd779e90b6ccadb7706abf4d26861d333
Adam Lovatt <adam.lovatt@vizworx.com>
2019-09-24 11:26:30 -0600

    feat(Utilities): adds an alternative handleInteraction that doesn't prevent default behaviours

--- src/utilities/handleInteraction.js
+++ src/utilities/handleInteraction.js
@@ -32,3 +32,33 @@ export default memoize((callback, ...boundArgs) => {
     .join('-');
   return `${memoizeReference(callback)}-${argIds}`;
 });
+
+// In some cases, such as reporting analytics when the user clicks a link, we
+// don't actually want to prevent the default behaviours
+export const handleUnblockedInteraction = memoize((callback, ...boundArgs) => {
+  // If no callback was passed, return an empty object to allow spreading props
+  if (!callback) { return {}; }
+
+  return {
+    onClick: (e) => {
+      callback(...boundArgs, e);
+    },
+    onKeyPress: (e) => {
+      if (e.key === 'Enter') {
+        callback(...boundArgs, e);
+      }
+    },
+    tabIndex: 0,
+    focusable: true,
+  };
+}, (callback, ...boundArgs) => {
+  const argIds = boundArgs
+    .map((v) => {
+      if (typeof v === 'function' || typeof v === 'object') {
+        return `REF${memoizeReference(v)}`;
+      }
+      return v;
+    })
+    .join('-');
+  return `${memoizeReference(callback)}-${argIds}`;
+});
